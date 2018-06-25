48d459a162e1a3ce5a14aee6158b94be7ba2c3fe
Austin Tam <austin.tam7@gmail.com>
2018-06-24 20:48:19 -0400

    add LwA button

--- index.php
+++ index.php
@@ -36,9 +36,33 @@
 </head>
 
 <body>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>  
+	<div id="amazon-root"></div>
+	<script type="text/javascript">
+		window.onAmazonLoginReady = function() {
+			amazon.Login.setClientId('amzn1.application-oa2-client.4246e0f086e441259742c758f63ca0bf');
+		};
+		(function(d) {
+			var a = d.createElement('script'); a.type = 'text/javascript';
+			a.async = true; a.id = 'amazon-login-sdk';
+			a.src = 'https://api-cdn.amazon.com/sdk/login1.js';
+			d.getElementById('amazon-root').appendChild(a);
+		})(document);
+	</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 <?php require_once './includes/nav.inc.php'; ?>
+
+<a href id="LoginWithAmazon">
+	<img border="0" alt="Login with Amazon" src="https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_312x64.png" width="312" height="64" />
+</a>
+<script type="text/javascript">
+    document.getElementById('LoginWithAmazon').onclick = function() {
+    options = { scope : 'profile' };
+    amazon.Login.authorize(options,
+        'https://ppcology.io/');
+    return false;
+    };
+</script>
 
 <header id="header">
   <div class="container">
