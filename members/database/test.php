aef79be813800bf367f58545e71f1e0815693f21
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-08-08 16:37:52 -0600

    asdf

--- members/database/test.php
+++ members/database/test.php
@@ -5,7 +5,4 @@ $sql = "SELECT * FROM users WHERE email='fernandgee@gmail.com'";
 $stmt = $pdo->query($sql);
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo '<pre>
-' . var_dump($result);
- . '
-</pre>';
+echo '<pre>' . var_dump($result) . '</pre>';
