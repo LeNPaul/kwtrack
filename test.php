d7a0eac39e9073a62117258b644d878edcc4f293
Fernand <fernandgee@gmail.com>
2018-08-14 12:51:46 -0600

    asdf

--- test.php
+++ test.php
@@ -11,8 +11,9 @@ $options = array(
 );
 
 curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_POST, true);
-//curl_setopt($curl, CURLOPT_HTTPHEADER, $options);
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $options);
 
 $result = curl_exec($curl);
 
