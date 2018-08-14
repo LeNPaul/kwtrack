e57b40bed8a7ff1980a8ea25ee8ad3ac772e44d1
Fernand <fernandgee@gmail.com>
2018-08-14 13:16:31 -0600

    asdf

--- members/includes/dashpages/profile/refresh.php
+++ members/includes/dashpages/profile/refresh.php
@@ -1,7 +1,9 @@
 <?php
+
 $curl = curl_init();
 
-$authCode = 'ANRsaNRYKILwPYCJpxst';
+$authCode = $_GET['code'];
+echo $authCode;
 $url = 'https://api.amazon.com/auth/o2/token';
 $data = 'grant_type=authorization_code&code=' . $authCode . '&redirect_uri=https://ppcology.io/&client_id=amzn1.application-oa2-client.4246e0f086e441259742c758f63ca0bf&client_secret=9c9e07b214926479e14a0781051ecc3ad9b29686d3cef24e15eb130a47cabeb3';
 
@@ -20,6 +22,3 @@ $result = curl_exec($curl);
 curl_close($curl);
 
 var_dump($result);
-
-
-
