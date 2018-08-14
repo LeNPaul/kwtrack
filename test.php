ccf6a8c78eba5d920dee10678e51ccdce6ef6f93
Fernand <fernandgee@gmail.com>
2018-08-14 12:09:12 -0600

    asdf

---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt($curl, CURLOPT_URL, 'https://ppcology.io/?code=ANJnRSvqXVROYHyktFmE&scope=cpc_advertising%3Acampaign_management&state=208257577110975193121591895857093449424');
+curl_exec($curl);
