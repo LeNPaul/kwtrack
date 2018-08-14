25dced94a35a71c5b7b149401bb01518a08e0ca5
Fernand <fernandgee@gmail.com>
2018-08-14 13:09:12 -0600

    asdf;

--- test.php
+++ test.php
@@ -4,7 +4,8 @@ $curl = curl_init();
 
 //curl_setopt($curl, CURLOPT_URL, 'https://ppcology.io/?code=ANJnRSvqXVROYHyktFmE&scope=cpc_advertising%3Acampaign_management&state=208257577110975193121591895857093449424');
 $authCode = 'ANtzibDMdXpwdmgiTbOD';
-$url = 'https://api.amazon.com/auth/o2/token?grant_type=authorization_code&code=' . $authCode . '&redirect_uri=https://ppcology.io/&client_id=amzn1.application-oa2-client.4246e0f086e441259742c758f63ca0bf&client_secret=9c9e07b214926479e14a0781051ecc3ad9b29686d3cef24e15eb130a47cabeb3';
+$url = 'https://api.amazon.com/auth/o2/token';
+$data = 'grant_type=authorization_code&code=' . $authCode . '&redirect_uri=https://ppcology.io/&client_id=amzn1.application-oa2-client.4246e0f086e441259742c758f63ca0bf&client_secret=9c9e07b214926479e14a0781051ecc3ad9b29686d3cef24e15eb130a47cabeb3';
 
 $options = array(
   'Content-Type:application/x-www-form-urlencoded;charset=UTF-8'
@@ -14,6 +15,8 @@ curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_HTTPHEADER, $options);
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 
 $result = curl_exec($curl);
 
