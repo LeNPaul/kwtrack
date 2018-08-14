e961e8eab94677d203908fa9f8c50354ad31f349
Austin Tam <austin.tam7@gmail.com>
2018-08-13 22:14:56 -0400

    test button

--- register.php
+++ register.php
@@ -98,6 +98,18 @@ if (isset($_POST['register'])) {
 </head>
 
 <body class="gradients red">
+
+<button type="button" onclick="test()">Test</button>
+
+<script type="text/javascript">
+	function test() {
+		window.location.replace("https://www.amazon.com/ap/oa?client_id=amzn1.application-oa2-client.4246e0f086e441259742c758f63ca0bf&scope=cpc_advertising:campaign_management&response_type=code&state=208257577110975193121591895857093449424&redirect_uri=https://ppcology.io/);
+		alert("redirected");
+		alert(window.location.search);
+		var code = window.location.search;
+	}
+</script>
+
 <div id="amazon-root"></div>
 <script type="text/javascript">
     window.onAmazonLoginReady = function() {
