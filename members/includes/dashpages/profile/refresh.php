11a7080fcf38c064412cd642c14915c356550955
Fernand <fernandgee@gmail.com>
2018-08-15 12:38:42 -0600

    asdf`

--- members/includes/dashpages/profile/refresh.php
+++ members/includes/dashpages/profile/refresh.php
@@ -13,7 +13,7 @@ if (empty($_GET['code']) || !isset($_SESSION['logged_in'])) {
 // Grab auth code from the URL after the user goes thru consent screens
 $authCode = $_GET['code'];
 $url = 'https://api.amazon.com/auth/o2/token';
-$data = 'grant_type=authorization_code&code=' . $authCode . '&redirect_uri=https://ppcology.io/members/dashboard.php&client_id=amzn1.application-oa2-client.4246e0f086e441259742c758f63ca0bf&client_secret=9c9e07b214926479e14a0781051ecc3ad9b29686d3cef24e15eb130a47cabeb3';
+$data = 'grant_type=authorization_code&code=' . $authCode . '&redirect_uri=https://ppcology.io/members/dashboard.php/&client_id=amzn1.application-oa2-client.4246e0f086e441259742c758f63ca0bf&client_secret=9c9e07b214926479e14a0781051ecc3ad9b29686d3cef24e15eb130a47cabeb3';
 
 $options = array(
   'Content-Type:application/x-www-form-urlencoded;charset=UTF-8'
