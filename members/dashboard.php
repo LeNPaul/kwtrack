6249e527ec3934381319ddb86b86658d8a7c9c95
Fernand <fernandgee@gmail.com>
2018-08-15 18:57:32 -0600

    asdf

--- members/dashboard.php
+++ members/dashboard.php
@@ -97,7 +97,7 @@ checkLoggedIn();
                 </div>
               </li>
               <li class="nav-item btn-rotate dropdown">
-                <a class="nav-link dropdown-toggle" href="#">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   <i class="nc-icon nc-settings-gear-65"></i>
                   <p>
                     <span class="d-lg-none d-md-block">Account</span>
