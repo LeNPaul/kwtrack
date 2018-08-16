63103d12933f579e65ee616447a6555154952eaf
Fernand <fernandgee@gmail.com>
2018-08-15 19:48:01 -0600

    asdf

--- members/includes/dashpages/main/dashboard_active2.php
+++ members/includes/dashpages/main/dashboard_active2.php
@@ -11,7 +11,8 @@ require '../../../ppc/AmazonAdvertisingApi/Client.php';
 echo 'asdfasdfasdf';
 
 // Grab refresh token from db
-$sql = "SELECT refresh_token FROM users WHERE user_id=$_SESSION['user_id']";
+$user_id = $_SESSION['user_id'];
+$sql = "SELECT refresh_token FROM users WHERE user_id='$user_id'";
 $stmt = $pdo->query($sql);
 $result = $stmt->fetchAll(PDO::FETCH_COLUMN);
 var_dump($result);
