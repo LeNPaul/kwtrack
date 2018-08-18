b91fbe542fec5b873270350f3ecb9212e19041bd
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-08-18 15:34:12 -0700

    asdf

---
+++ members/includes/dashpages/main/dashboard_active4.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ *  User will see this after selecting their profile. Will show this page until all of their data has finished importing.
+ */
+
+?>
+
+<div class="container text-center">
+  <h2>Your campaign data is currently importing</h2>
+  <p>Please check back in a few hours.</p>
+</div>
