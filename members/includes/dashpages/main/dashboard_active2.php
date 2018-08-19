04fbb0babedf246100081beb49279b4fb4d1966b
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-08-19 14:02:40 -0700

    asdf

--- members/includes/dashpages/main/dashboard_active2.php
+++ members/includes/dashpages/main/dashboard_active2.php
@@ -13,14 +13,16 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/members/node_modules/hquery.php/hquer
  */
 
 function displayProfiles($profiles) {
-  // Scrape Amazon to grab seller name
-  $url = 'https://www.amazon.com/sp?seller=' . $profiles[0]['accountInfo']['sellerStringId'];
-  $doc = hQuery::fromUrl($url, ['Accept' => 'text/html,application/xhtml+xml;q=0.9,*/*;q=0.8']);
-  $sellerName = $doc->find("h1#sellerName");
+
   $output = '';
   var_dump($profiles);
   // Iterate through profiles and append block level buttons to $output
   for ($i = 0; $i < count($profiles); $i++) {
+    // Scrape Amazon to grab seller name
+    $url = 'https://www.amazon.com/sp?seller=' . $profiles[$i]['accountInfo']['sellerStringId'];
+    $doc = hQuery::fromUrl($url, ['Accept' => 'text/html,application/xhtml+xml;q=0.9,*/*;q=0.8']);
+    $sellerName = $doc->find("h1#sellerName");
+
     // Find out which country the profile is from so we can put the country flag in the button
     if ($profiles[$i]['countryCode'] == 'US') {
       // Set USA flag
