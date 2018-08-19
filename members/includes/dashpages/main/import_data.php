90d3f7b38b6688614da4eb6ce36c0426f4ff8d9f
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-08-19 15:30:06 -0700

    commit

--- members/includes/dashpages/main/import_data.php
+++ members/includes/dashpages/main/import_data.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL); ini_set("error_reporting", E_ALL);
 namespace AmazonAdvertisingApi;
+error_reporting(E_ALL); ini_set("error_reporting", E_ALL);
 require '../../../database/pdo.inc.php';
 require_once '../../AmazonAdvertisingApi/Client.php';
 
