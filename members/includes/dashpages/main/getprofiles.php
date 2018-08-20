70790223408d41bad8b76fc452b7cb29c9dd75d2
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-08-20 14:20:25 -0700

    commit

--- members/includes/dashpages/main/getprofiles.php
+++ members/includes/dashpages/main/getprofiles.php
@@ -23,8 +23,8 @@ $refreshToken = $result[0]['refresh_token'];
 
 // Run campaign importing in background [FIX WHEN YOU COME BACK]
 $user_id = $_SESSION['user_id'];
-$cmd = "php ~/public_html/members/includes/dashpages/main/import_data.php {$refreshToken} {$user_id} {$profileId}";
-launchBackgroundProcess($cmd);
+$cmd = "exec php ~/public_html/members/includes/dashpages/main/import_data.php {$refreshToken} {$user_id} {$profileId} > /dev/null &";
+exec($cmd);
 
 //shell_exec("php import_data.php $refreshToken $user_id $profileId > /dev/null &");
 
