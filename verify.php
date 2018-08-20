b54c6a0cd507ac5efc2e1347bbe41b06fa0ac32a
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-08-20 15:19:26 -0700

    commit

--- verify.php
+++ verify.php
@@ -29,7 +29,7 @@ if ((isset($_GET['email']) && !empty($_GET['email'])) && (isset($_GET['hash']) &
     $stmt->execute(array("email" => $email));
     $_SESSION['active'] = 1;
 
-    header("location: https://ppcology.io/members/dashboard.php");
+    header("location: https://ppcology.io/members/login.php");
     exit();
   }
 } else {
