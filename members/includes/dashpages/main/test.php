e91a1c7526694030bd32346cc7530ad198b3a737
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-08-20 16:28:32 -0700

    commit

--- members/includes/dashpages/main/test.php
+++ members/includes/dashpages/main/test.php
@@ -1,12 +1,30 @@
 <?php
-var_dump($_POST);
+namespace AmazonAdvertisingApi;
+require_once '../../AmazonAdvertisingApi/Client.php';
 
- ?>
+$config = array(
+  "clientId" => "amzn1.application-oa2-client.4246e0f086e441259742c758f63ca0bf",
+  "clientSecret" => "9c9e07b214926479e14a0781051ecc3ad9b29686d3cef24e15eb130a47cabeb3",
+  "refreshToken" => $refreshToken,
+  "region" => "na",
+  "sandbox" => false,
+  );
+$client = new Client($config);
+$client->profileId = $profileId;
 
- <form method="post">
+$result = $client->requestReport(
+  "campaigns",
+  array("reportDate"    => "20180810",
+        "campaignType"  => "sponsoredProducts",
+        "metrics"       => "campaignName, campaignId, campaignStatus,
+                            campaignBudget, impressions, clicks, cost,
+                            attributedConversions30d, attributedUnitsOrdered30d,
+                            attributedSales30d"
+  )
+);
 
-   <input type="submit" name="asdf[]" value="asdfasdf" />
-   <input type="submit" name="asdf[]" value="asdf" />
-   <input type="submit" name="asdf[]" value="a" />
-   <input type="submit" name="asdf[]" value="asdfa" />
- </form>
+echo '<pre>';
+var_dump($result);
+echo '</pre>';
+
+?>
