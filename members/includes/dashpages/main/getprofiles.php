905eb2ea6f44bec3f53fbcbcba1d4f9e83b1d14d
Austin Tam <austin.tam7@gmail.com>
2018-08-20 23:57:04 -0400

    test getprofiles

--- members/includes/dashpages/main/getprofiles.php
+++ members/includes/dashpages/main/getprofiles.php
@@ -27,8 +27,9 @@ echo 'refresh token got';
 
 // Run campaign importing in background [FIX WHEN YOU COME BACK]
 $user_id = $_SESSION['user_id'];
-$cmd = "php ~/members/includes/dashpages/main/import_data.php $refreshToken $user_id $profileId";
-echo exec($cmd);
+exec("php ~/public_html/members/includes/dashpages/main/import_data.php $refreshToken $user_id $profileId");
+
+echo 'finished';
 
 //shell_exec("php import_data.php $refreshToken $user_id $profileId > /dev/null &");
 
