ec48beba3ddb82ca715db653a03e46025509692b
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-08-21 14:43:35 -0700

    commit

--- members/includes/dashpages/main/test.php
+++ members/includes/dashpages/main/test.php
@@ -26,7 +26,8 @@ $result = $client->requestReport(
 //$result = $client->getReport("amzn1.clicksAPI.v1.p1.5B7C72BF.3e172900-f3de-4bab-94d5-6f73ab22a178");
 
 echo '<pre>';
-var_dump(json_decode($result, true));
+var_dump($result);
+// var_dump(json_decode($result, true));
 echo '</pre>';
 
 /*
