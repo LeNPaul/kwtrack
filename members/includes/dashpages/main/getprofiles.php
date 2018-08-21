530e5474ab8e5dbad5d4a1ea3af0421337b4c6c0
Austin Tam <austin.tam7@gmail.com>
2018-08-20 23:05:06 -0400

    test getprofiles

--- members/includes/dashpages/main/getprofiles.php
+++ members/includes/dashpages/main/getprofiles.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL); ini_set("error_reporting", E_ALL);
 
 // Insert profileID in database for the user and set active level to 3
 $profileId = $_POST['selectedProfile'];
-$profileId = $profileId[0];
+$profileId = escapeshellarg($profileId[0]);
 $sql = 'UPDATE users SET profileId=:profileId, active=:level WHERE user_id=:user_id';
 $stmt = $pdo->prepare($sql);
 $stmt->execute(array(
@@ -19,11 +19,11 @@ $stmt->execute(array(
 $sql = 'SELECT refresh_token FROM users WHERE user_id=' . $_SESSION['user_id'];
 $stmt = $pdo->query($sql);
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$refreshToken = $result[0]['refresh_token'];
+$refreshToken = escapeshellarg($result[0]['refresh_token']);
 
 // Run campaign importing in background [FIX WHEN YOU COME BACK]
-$user_id = $_SESSION['user_id'];
-$cmd = "php ~/public_html/members/includes/dashpages/main/import_data.php {$refreshToken} {$user_id} {$profileId} 2>&1";
+$user_id = escapeshellarg($_SESSION['user_id']);
+$cmd = "php ~/public_html/members/includes/dashpages/main/import_data.php $refreshToken $user_id $profileId 2>&1";
 $execResult = shell_exec($cmd);
 var_dump($execResult);
 
