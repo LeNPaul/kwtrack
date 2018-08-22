9e13284f64af5ca7614c24c31d46c22e660100e5
Austin Tam <austin.tam7@gmail.com>
2018-08-22 02:07:33 -0400

    test getprofiles

--- members/includes/dashpages/main/import_data.php
+++ members/includes/dashpages/main/import_data.php
@@ -20,13 +20,11 @@ $config = array(
   );
 $client = new Client($config);
 $client->profileId = $profileId;
-echo "finished";
 
 // First, grab campaigns and store them in db
 $result = $client->listCampaigns(array("stateFilter" => "enabled", "stateFilter" => "paused"));
 $campJson = $result['response'];
 $campaigns = json_decode($campJson, true);
-echo count($campaigns);
 
 // Iterate through campaign array and add them to db w/ foreign key user_id
 for ($i = 0; $i < count($campaigns); $i++) {
@@ -48,10 +46,6 @@ for ($i = 0; $i < count($campaigns); $i++) {
 $result = $client->listAdGroups(array("stateFilter" => "enabled", "stateFilter" => "paused"));
 $adgrJson = $result['response'];
 $adgroups = json_decode($adgrJson, true);
-
-echo '<pre>';
-var_dump($adgroups);
-echo '</pre>';
 
 for ($i = 0; $i < count($adgroups); $i++) {
   $sql = 'INSERT INTO ad_groups
