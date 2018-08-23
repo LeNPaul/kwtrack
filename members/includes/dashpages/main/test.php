fc6e87d701fb5bcd24fba44b9e0db221e66cf51a
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-08-22 17:24:15 -0700

    commit

--- members/includes/dashpages/main/test.php
+++ members/includes/dashpages/main/test.php
@@ -19,7 +19,7 @@ $config = array(
   "clientSecret" => "9c9e07b214926479e14a0781051ecc3ad9b29686d3cef24e15eb130a47cabeb3",
   "refreshToken" => $refreshToken,
   "region" => "na",
-  "sandbox" => true,
+  "sandbox" => false,
   );
 $client = new Client($config);
 $client->profileId = $profileId;
