288bada7c0673dec1078cd71ce37f8798517add6
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-08-23 16:02:25 -0700

    commit

--- members/includes/dashpages/helper.inc.php
+++ members/includes/dashpages/helper.inc.php
@@ -48,22 +48,4 @@ function getRefreshToken($pdo, $user_id) {
   $result = $stmt->fetchAll(PDO::FETCH_COLUMN);
   var_dump($result);
 }
-
-function launchBackgroundProcess($command) {
-  // Run command Asynchroniously (in a separate thread)
-  if(PHP_OS=='WINNT' || PHP_OS=='WIN32' || PHP_OS=='Windows'){
-    // Windows
-    $command = 'start "" '. $command;
-  } else {
-    // Linux/UNIX
-    $command = $command .' /dev/null &';
-  }
-  $handle = popen($command, 'r');
-  if($handle!==false){
-    pclose($handle);
-    return true;
-  } else {
-    return false;
-  }
-}
 ?>
