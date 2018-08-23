60f8390f616b0759c9d0629aa6c48ce4bdfd1e57
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-08-23 14:24:49 -0700

    commit

--- members/includes/dashpages/main/getprofiles.php
+++ members/includes/dashpages/main/getprofiles.php
@@ -4,6 +4,7 @@ require '../../../database/pdo.inc.php';
 require '../helper.inc.php';
 error_reporting(E_ALL); ini_set("error_reporting", E_ALL);
 
+/*
 // Insert profileID in database for the user and set active level to 3
 $profileId = $_POST['selectedProfile'];
 $profileId = $profileId[0];
@@ -20,9 +21,9 @@ $sql = 'SELECT refresh_token FROM users WHERE user_id=' . $_SESSION['user_id'];
 $stmt = $pdo->query($sql);
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $refreshToken = $result[0]['refresh_token'];
-
+*/
 // Run campaign importing in background [FIX WHEN YOU COME BACK]
-$user_id = $_SESSION['user_id'];
+//$user_id = $_SESSION['user_id'];
 echo 'running exec <br />';
 exec("php import_data.php > /dev/null 2>&1");
 echo 'finished running exec <br />';
