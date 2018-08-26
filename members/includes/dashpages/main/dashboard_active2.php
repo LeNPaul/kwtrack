847d307580a0ebf847ea688875623620e1ab2c4b
Austin Tam <austin.tam7@gmail.com>
2018-08-25 21:18:44 -0400

    test getprofile

--- members/includes/dashpages/main/dashboard_active2.php
+++ members/includes/dashpages/main/dashboard_active2.php
@@ -55,7 +55,7 @@ function displayProfiles($profiles) {
 					type: 'POST',
 					url: 'includes/dashpages/main/getprofiles.php',
 					success: function(data) {
-						alert("success");
+						alert(data.responseText);
 					},
 					error: function(msg, xhr) {
 						alert(msg + ", " + xhr.responseText);
