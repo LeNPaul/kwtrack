5a3cb9f73601373deb030dcfcdee82fa030a9ba6
Austin Tam <austin.tam7@gmail.com>
2018-08-25 21:08:20 -0400

    test getprofile

--- members/includes/dashpages/main/dashboard_active2.php
+++ members/includes/dashpages/main/dashboard_active2.php
@@ -56,6 +56,9 @@ function displayProfiles($profiles) {
 					success: function(data) {
 						alert("success");
 					}
+					error: function(msg, xhr) {
+						alert(msg + ", " + xhr.responseText);
+					}
 				});
 			});
 		});
