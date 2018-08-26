be5cdc1e9ed4de503eaf8666454d153636e29643
Austin Tam <austin.tam7@gmail.com>
2018-08-25 21:25:28 -0400

    test getprofile

--- members/includes/dashpages/helper.inc.php
+++ members/includes/dashpages/helper.inc.php
@@ -44,7 +44,7 @@
 
  function storeAdGroupArrays($pdo, $dbVar, $arrAdGroupIds, $dbColName) {
     for ($i = 0; $i < count(arrAdGroupIds); $i++) {
-        $sql "UPDATE ad_groups SET {$dbColName}=:value WHERE amz_adgroup_id=:amz_adgroup_id";
+        $sql 'UPDATE ad_groups SET $dbColName=:value WHERE amz_adgroup_id=:amz_adgroup_id';
         $stmt = $pdo->prepare($sql);
         $stmt->execute(array(
             ':value'            => serialize(dbVar[$i]),
