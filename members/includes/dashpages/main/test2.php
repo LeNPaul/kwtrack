40770d0d85f9c2add86c9a17a1ab90927f7ad90a
Fernand Gonzales <fernandgonzales@Fernands-MBP.hitronhub.home>
2018-08-28 12:56:39 -0600

    commit

--- members/includes/dashpages/main/test2.php
+++ members/includes/dashpages/main/test2.php
@@ -1,6 +1,7 @@
 <?php
 $arr = [1,1,1,1,1];
-$arr = array_reduce($arr, function($carry, $element) { return $carry += $element }, '');
+
+$arr = array_reduce($arr, function ($carry, $element) { return $carry += $element; });
 
 echo $arr;
 ?>
