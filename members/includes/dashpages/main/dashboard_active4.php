f24c0f20408189c389eb91e0d97a58ff7b38e5e2
Fernand <fernandgee@gmail.com>
2018-08-28 16:40:49 -0600

    asdf

--- members/includes/dashpages/main/dashboard_active4.php
+++ members/includes/dashpages/main/dashboard_active4.php
@@ -8,54 +8,54 @@ require './includes/dashpages/helper.inc.php';
 
 ?>
 
-<div class="container">
-  <!--  Row for PPC metrics: ad spend, ppc sales, ppc ACoS -->
-  <div class="row">
-    <div class="card text-white bg-success mb-3 col-sm-4">
-      <!--    <div class="card-header">Header</div>-->
-      <div class="card-body">
-        <h5 class="card-title">Ad Spend</h5>
-        <p class="card-text">test</p>
-      </div>
-    </div>
-  
-    <div class="card text-white bg-success mb-3 col-sm-4">
-      <div class="card-body">
-        <h5 class="card-title">PPC Sales</h5>
-        <p class="card-text">test</p>
-      </div>
-    </div>
-  
-    <div class="card text-white bg-success mb-3 col-sm-4">
-      <div class="card-body">
-        <h5 class="card-title">PPC ACoS</h5>
-        <p class="card-text">test</p>
-      </div>
+
+<!--  Row for PPC metrics: ad spend, ppc sales, ppc ACoS -->
+<div class="row">
+  <div class="card text-white bg-success mb-3 col-sm-4">
+    <!--    <div class="card-header">Header</div>-->
+    <div class="card-body">
+      <h5 class="card-title">Ad Spend</h5>
+      <p class="card-text">test</p>
     </div>
   </div>
-  
-  <!--  Row for organic metrics: true ACoS, total sales, organic sales -->
-  <div class="row">
-    <div class="card text-white bg-success mb-3 col-sm-4">
-      <!--    <div class="card-header">Header</div>-->
-      <div class="card-body">
-        <h5 class="card-title">True ACoS</h5>
-        <p class="card-text">test</p>
-      </div>
+
+  <div class="card text-white bg-success mb-3 col-sm-4">
+    <div class="card-body">
+      <h5 class="card-title">PPC Sales</h5>
+      <p class="card-text">test</p>
     </div>
-  
-    <div class="card text-white bg-success mb-3 col-sm-4;">
-      <div class="card-body">
-        <h5 class="card-title">Total Sales</h5>
-        <p class="card-text">test</p>
-      </div>
-    </div>
-  
-    <div class="card text-white bg-success mb-3 col-sm-4">
-      <div class="card-body">
-        <h5 class="card-title">Organic Sales</h5>
-        <p class="card-text">test</p>
-      </div>
+  </div>
+
+  <div class="card text-white bg-success mb-3 col-sm-4">
+    <div class="card-body">
+      <h5 class="card-title">PPC ACoS</h5>
+      <p class="card-text">test</p>
     </div>
   </div>
 </div>
+
+<!--  Row for organic metrics: true ACoS, total sales, organic sales -->
+<div class="row">
+  <div class="card text-white bg-success col-sm-4">
+    <!--    <div class="card-header">Header</div>-->
+    <div class="card-body">
+      <h5 class="card-title">True ACoS</h5>
+      <p class="card-text">test</p>
+    </div>
+  </div>
+
+  <div class="card text-white bg-success col-sm-4;">
+    <div class="card-body">
+      <h5 class="card-title">Total Sales</h5>
+      <p class="card-text">test</p>
+    </div>
+  </div>
+
+  <div class="card text-white bg-success col-sm-4">
+    <div class="card-body">
+      <h5 class="card-title">Organic Sales</h5>
+      <p class="card-text">test</p>
+    </div>
+  </div>
+</div>
+
