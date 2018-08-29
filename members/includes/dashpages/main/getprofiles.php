bd48438aa30e660b356db22e11f8aa05c6a7ba54
Austin Tam <austin.tam7@gmail.com>
2018-08-28 22:28:55 -0400

    test background

--- members/includes/dashpages/main/getprofiles.php
+++ members/includes/dashpages/main/getprofiles.php
@@ -10,7 +10,12 @@ $executable = '~/opt/cpanel/ea-php56/root/usr/bin/php.exe';
 $path = '~/public_html/members/include/dashpages/main/import_data.php';
 
 exec($executable." ".$path, $output, $return);
-echo 'doneprofiles';
+echo exec_enabled();
+
+function exec_enabled() {
+  $disabled = explode(',', ini_get('disable_functions'));
+  return !in_array('exec', $disabled);
+}
 
 /*
 // Insert profileID in database for the user and set active level to 3
