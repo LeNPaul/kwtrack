90a44c27842cce831bdd31e0f9cdc12c7b22048a
Austin Tam <austin.tam7@gmail.com>
2018-08-30 20:58:48 -0400

    import adgroup

--- members/includes/dashpages/main/test.php
+++ members/includes/dashpages/main/test.php
@@ -247,10 +247,9 @@ for ($i = 0; $i < 60; $i++) {
     $result = json_decode($result['response'], true);
 
     for ($x = 0; $x < count($result); $x++) {
-	  print_r ($result[$x]['adGroupId']);
   	  $extra = $client->getAdGroup($result[$x]['adGroupId']);
 	  print_r($extra);
-  	  $extraArray[] = json_decode($extra, true);
+  	  $extraArray[] = json_decode($extra['response'], true);
 	  print_r($extraArray);
       $sql = 'INSERT INTO ad_groups (user_id, status, default_bid, amz_adgroup_id, amz_campaign_id, ad_group_name)
               VALUES (:user_id, :status, :default_bid, :adgroup_id, :amz_campaign_id, :adgroup_name)';
