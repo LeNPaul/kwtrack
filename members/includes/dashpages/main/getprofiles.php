59168ae4739a7e2bac0bd300ce152a0ed148c581
Austin Tam <austin.tam7@gmail.com>
2018-08-30 23:49:10 -0400

    bg import

--- members/includes/dashpages/main/getprofiles.php
+++ members/includes/dashpages/main/getprofiles.php
@@ -6,12 +6,6 @@ require '../../../database/pdo.inc.php';
 require_once '../../AmazonAdvertisingApi/Client.php';
 error_reporting(E_ALL); ini_set("error_reporting", E_ALL);
 
-$sql = 'INSERT INTO users (first_name) VALUES (:first_name)';
-$stmt = $pdo->prepare($sql);
-$stmt->execute(array(':first_name' => "alfksjdhfldskjfhsdflkjh"));
-
-
-/*
 // Insert profileID in database for the user and set active level to 3
 $profileId = $_POST['selectedProfile'];
 $profileId = $profileId[0];
@@ -28,58 +22,30 @@ $sql = 'SELECT refresh_token FROM users WHERE user_id=' . $_SESSION['user_id'];
 $stmt = $pdo->query($sql);
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $refreshToken = $result[0]['refresh_token'];
-*/
-// Run campaign importing in background [FIX WHEN YOU COME BACK]
-//$user_id = $_SESSION['user_id'];
+$user_id = $_SESSION['user_id'];
+
+import_data();
 
 
+function import_data(){
+	// TODO: integrate campaign, adgroup, and keyword import code from test.php here
+	// Instantiate client for advertising API
+	$config = array(
+		"clientId" => "amzn1.application-oa2-client.4246e0f086e441259742c758f63ca0bf",
+		"clientSecret" => "9c9e07b214926479e14a0781051ecc3ad9b29686d3cef24e15eb130a47cabeb3",
+		"refreshToken" => $refreshToken,
+		"region" => "na",
+		"sandbox" => false,
+	);
+	$client = new Client($config);
+	$client->profileId = $profileId;
 
+	// First, grab campaigns and store them in db
+	
 
-/**
-* Launch Background Process
-*
-* Launches a background process (note, provides no security itself, $call must be sanitized prior to use)
-* @param string $call the system call to make
-* @author raccettura
-*/
+	// Second, grab ad groups and store them in db
 
-/*
-function launchBackgroundProcess($call) {
-    // Windows
-    if(is_windows()){
-        pclose(popen('start /b '.$call, 'r'));
-    }
-    // Some sort of UNIX
-    else {
-        pclose(popen($call.' /dev/null &', 'r'));
-    }
-    return true;
 }
-*/
-
-/**
-* Is Windows
-*
-* Tells if we are running on Windows Platform
-* @author raccettura
-*/
-
-/*
-function is_windows(){
-    if(PHP_OS == 'WINNT' || PHP_OS == 'WIN32'){
-        return true;
-    }
-    return false;
-}
-
-echo 'running exec <br /><br />';
-//exec("php import_data.php > /dev/null 2>&1");
-//launchBackgroundProcess("php import_data.php");
-pclose(popen('php import_data.php /dev/null &', 'r'));
-echo 'finished running exec <br />';
-
-
-//shell_exec("php import_data.php $refreshToken $user_id $profileId > /dev/null &");
 
 // Redirect to dashboard
 // header('location: ../../../dashboard.php');
