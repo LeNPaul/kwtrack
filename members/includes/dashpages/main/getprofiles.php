a5cdf6fd773ea45764b23ed73548f0d55882b4ed
Austin Tam <austin.tam7@gmail.com>
2018-09-06 14:01:24 -0400

    getprofiles

--- members/includes/dashpages/main/getprofiles.php
+++ members/includes/dashpages/main/getprofiles.php
@@ -6,7 +6,6 @@ require '../helper.inc.php';
 require_once '../../AmazonAdvertisingApi/Client.php';
 error_reporting(E_ALL); ini_set("error_reporting", E_ALL);
 use PDO;
-
 // Insert profileID in database for the user and set active level to 3
 $profileId = $_POST['selectedProfile'];
 //$profileId = $profileId[0];
@@ -17,12 +16,10 @@ $stmt->execute(array(
   ':level'     => 3,
   ':user_id'    => $_SESSION['user_id']
 ));
-
 // Get refresh token
 $sql = 'SELECT refresh_token FROM users WHERE user_id=' . $_SESSION['user_id'];
 $stmt = $pdo->query($sql);
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 $refreshToken = $result[0]['refresh_token'];
 $user_id = $_SESSION['user_id'];
 // Redirect to dashboard
