b12e6c70349d20729f8449ff99e377c9f34dd947
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-09 16:52:48 -0400

    asdf

--- members/includes/dashpages/main/dashboard_active3.php
+++ members/includes/dashpages/main/dashboard_active3.php
@@ -12,13 +12,15 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script type="text/javascript">
+var user_id = "<?= $_SESSION['user_id'] ?>";
 	$(document).ready(function(){
 		$.ajax({
 			type: 'POST',
 			dataType: 'text',
+			data:
 			url: 'includes/dashpages/main/import_data.php',
 			success: function() {
-				alert('success');
+				alert('user id: ' . user_id);
 			},
 			error: function(msg, xhr) {
 				alert(msg + ", " + xhr.responseText);
