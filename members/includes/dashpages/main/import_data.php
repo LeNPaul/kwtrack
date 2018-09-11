576eeab5c2c108a0a57d2f2b2514ffcdc4791c40
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-11 14:11:18 -0400

    asdf

--- members/includes/dashpages/main/import_data.php
+++ members/includes/dashpages/main/import_data.php
@@ -21,7 +21,8 @@ $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $refreshToken = $result[0]['refresh_token'];
 $profileId = $result[0]['profileId'];
 
-// TODO: integrate keyword import code from test.php here
+$refreshToken = 'Atzr|IwEBID8Cr8D51I4XzWRU5wdohHUoGJRY1rempo6uwgk_niC5AgqZo_SVul0Nt8V5oU1j8P2T08oPjR8gLKSsWnJuAflfBzcMky0NzBoKcSIYH62WJ4I86G6t4jGxU7fitoLO79TJPFjCoHPXyjnvaNLxFaJPxOaW3t4fLBH9-1RGsAaEdrP0-r85iVNgG_pQE2HA7bl_ZMqWoJbXhww-YEsfMH6tBKXG0S0dMreLkEkdx75eABfzKwdDm9jokTL8YZjkqj1ELRFOwK6Pgv1PsYTvdI2Us1fTw-Bu1n_n4am_vlrK4ntseK_dqFHvrV4_h0aup1hoChA5KZD2ID3fG4e4be4iCRC66QdJxmjv_q_o8RxoZR_bG0vhlkU2rSYKnMnZOj7nkRS2Z6JoRPWRLw7nP8nEfHLRkCQnrOn2PHkrKX7MWTIWt1f-_rkr3ocfvgKfcixFvTc6XmNGg0IYbVidw0thS3-AgSpnGaG0O7Q-W9VZPFRFtas1PltUG69LL0ko2EOz6yW-RG9071MfpUMgre2_TUildA68rlcdikXtNfMtyYNwqvQhlSZ_eVXWGclIpk4XQ39a-5eJiB8HVfsAvgdF';
+$profileId = '1215041354659387';
 
 // Instantiate client for advertising API
 $config = array(
@@ -88,6 +89,7 @@ for ($i = 0; $i < 60; $i++) {
 		// Get the report using the report id
 		$result = $client->getReport($reportId);
 		$result = json_decode($result['response'], true);
+		
 		var_dump($result);
 
 		for ($x = 0; $x < count($result); $x++) {
@@ -116,7 +118,7 @@ for ($i = 0; $i < 60; $i++) {
 		$result = json_decode($result['response'], true);
 		$reportId = $result['reportId'];
 
-		sleep(15);
+		sleep(10);
 
 		// Get the report using the report id
 		$result = $client->getReport($reportId);
