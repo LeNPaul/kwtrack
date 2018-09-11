387132653fd38774ed1efb6b6af62c369fb993c8
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-11 15:15:59 -0400

    asdf

--- members/includes/dashpages/main/test2.php
+++ members/includes/dashpages/main/test2.php
@@ -33,7 +33,7 @@ $result = $client->requestReport(
 $result = json_decode($result['response'], true);
 $reportId = $result['reportId'];
 
-sleep(20);
+sleep(30);
 
 // Get the report using the report id
 $result = $client->getReport($reportId);
