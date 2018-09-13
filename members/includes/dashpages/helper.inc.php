231f7da87975f075f70b9689fc2b23b08d0fee41
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-13 13:20:24 -0400

    asdf

--- members/includes/dashpages/helper.inc.php
+++ members/includes/dashpages/helper.inc.php
@@ -153,7 +153,7 @@
           echo '<pre>';
           var_dump($status);
           echo '<br /><br />' . $status['bid'];
-          echo '</pre>';
+          echo '</pre>';die;
           $bid = $status['bid'];
           $status = $status['state'];
 
