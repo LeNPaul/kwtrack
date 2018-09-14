04527893fb16f99f26738d03abd18644d1025513
Austin Tam <austin.tam7@gmail.com>
2018-09-14 13:00:28 -0400

    chart

--- members/includes/dashpages/main/dashboard_active5.php
+++ members/includes/dashpages/main/dashboard_active5.php
@@ -7,8 +7,10 @@
 require './includes/dashpages/helper.inc.php';
 
 // Grab metric data for all campaigns and store in an array for each metric
-$adSpend = calculateMetrics(multiUnserialize(getMetricData($pdo, 'ad_spend', 2)), 5, 'adSpend');
-$ppcSales = calculateMetrics(multiUnserialize(getMetricData($pdo, 'sales', 2)), 5, 'ppcSales');
+$adSpend = multiUnserialize(getMetricData($pdo, 'ad_spend', 2);
+$ppcSales = multiUnserialize(getMetricData($pdo, 'sales', 2);
+var_dump($adSpend);
+var_dump($ppcSales);
 
 echo '<pre>';
 multiUnserialize(getMetricData($pdo, 'ad_spend', 0));
