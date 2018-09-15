940c81e1d3b989e0b68dec1a6b155ff07c2ebe25
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-15 16:13:21 -0400

    asdf

--- members/includes/dashpages/main/dashboard_active5.php
+++ members/includes/dashpages/main/dashboard_active5.php
@@ -6,8 +6,8 @@
 require './includes/dashpages/helper.inc.php';
 
 // Grab metric data for all campaigns and store in an array for each metric
-$adSpendArr = calculateMetrics(multiUnserialize(getMetricData($pdo, 'ad_spend', $_SESSION['user_id'])), 15, 'ad_spend');
-$ppcSalesArr = calculateMetrics(multiUnserialize(getMetricData($pdo, 'sales', $_SESSION['user_id'])), 15,'ad_spend');
+$adSpendArr = calculateMetrics(multiUnserialize(getMetricData($pdo, 'ad_spend', $_SESSION['user_id'])), 1, 'ad_spend');
+$ppcSalesArr = calculateMetrics(multiUnserialize(getMetricData($pdo, 'sales', $_SESSION['user_id'])), 1,'ad_spend');
 
 $adSpend = array_sum($adSpendArr);
 $ppcSales = array_sum($ppcSalesArr);
