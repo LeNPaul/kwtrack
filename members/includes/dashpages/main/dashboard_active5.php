dae73d62dd675ac2049bc88d8654bb1ed5df5ae1
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-15 16:06:27 -0400

    asdf

--- members/includes/dashpages/main/dashboard_active5.php
+++ members/includes/dashpages/main/dashboard_active5.php
@@ -7,8 +7,8 @@
 require './includes/dashpages/helper.inc.php';
 
 // Grab metric data for all campaigns and store in an array for each metric
-$adSpendArr = calculateMetrics(multiUnserialize(getMetricData($pdo, 'ad_spend', 2)), 1, 'ad_spend');
-$ppcSalesArr = calculateMetrics(multiUnserialize(getMetricData($pdo, 'sales', 2)), 1,'ad_spend');
+$adSpendArr = array_sum(calculateMetrics(multiUnserialize(getMetricData($pdo, 'ad_spend', 2)), 1, 'ad_spend'));
+$ppcSalesArr = array_sum(calculateMetrics(multiUnserialize(getMetricData($pdo, 'sales', 2)), 1,'ad_spend'));
 
 echo '<pre>';
 multiUnserialize(getMetricData($pdo, 'ad_spend', 0));
@@ -197,17 +197,17 @@ if ($ppcSales == 0) {
 <canvas id="testChart" width="1000" height="400"></canvas>
 <script>
 var ctx = document.getElementById("testChart");
-var adSpendArr = <?php echo json_encode($adSpendArr); ?>;
+var adSpendArr = <?= json_encode($adSpendArr); ?>;
 alert(adSpendArr[0]);
-var ppcSalesArr = <?php echo json_encode($ppcSalesArr); ?>;
+var ppcSalesArr = <?= json_encode($ppcSalesArr); ?>;
 var ppcAcosArr = [];
 
 var data = {
-		labels: ["day1", "day2", "day3", "day4", "day5", "day6", "day7", "day8", "day9", "day10", "day11", "day12", "day13", "day14", "day15", "day16", "day17", "day18", "day19", "day20", "day21", "day22", "day23", "day24", "day25", "day26", "day27", "day28", "day29", "day30", "day31", "day32", "day33", "day34", "day35", "day36", "day37", "day38", "day39", "day40", "day41", "day42", "day43", "day44", "day45", "day46", "day47", "day48", "day49", "day50", "day51", "day52", "day53", "day54", "day55", "day56", "day57", "day58", "day59", "day60", ], 
+		labels: ["day1", "day2", "day3", "day4", "day5", "day6", "day7", "day8", "day9", "day10", "day11", "day12", "day13", "day14", "day15", "day16", "day17", "day18", "day19", "day20", "day21", "day22", "day23", "day24", "day25", "day26", "day27", "day28", "day29", "day30", "day31", "day32", "day33", "day34", "day35", "day36", "day37", "day38", "day39", "day40", "day41", "day42", "day43", "day44", "day45", "day46", "day47", "day48", "day49", "day50", "day51", "day52", "day53", "day54", "day55", "day56", "day57", "day58", "day59", "day60", ],
 		datasets: [{
-			label: "Ad Spend", 
-			data: adSpendArr, 
-			fill: false, 
+			label: "Ad Spend",
+			data: adSpendArr,
+			fill: false,
 			borderColor: "rgb(151, 253, 143)"
 		}, {
 			label: "PPC Sales",
