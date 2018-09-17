45ebfb2056b38a5f5b23cd9ae8983ba2c942cbaf
Austin Tam <austin.tam7@gmail.com>
2018-09-16 21:49:23 -0400

    dashboard5

--- members/includes/dashpages/main/dashboard_active5.php
+++ members/includes/dashpages/main/dashboard_active5.php
@@ -11,7 +11,6 @@ $ppcSalesArr = array_reverse(calculateMetrics(multiUnserialize(getMetricData($pd
 
 $adSpend = array_sum($adSpendArr);
 $ppcSales = array_sum($ppcSalesArr);
-
 $acos = [];
 
 for ($i = 0; $i < count($adSpendArr); $i++) {
@@ -20,6 +19,13 @@ for ($i = 0; $i < count($adSpendArr); $i++) {
 	} else {
 		$acos[] = round((double)($adSpendArr[$i] / $ppcSalesArr[$i]) * 100, 2);
 	}
+}
+
+$dateArr = [];
+$dateArr[] = date("d/m/Y");
+
+for ($j = 1; $j < 60; $j++) {
+	$dateArr[] = date("d/m/Y", strtotime("-".$j." days"));
 }
 
 ?>
@@ -203,9 +209,10 @@ var ctx = document.getElementById("testChart");
 var adSpendArr = <?= json_encode($adSpendArr); ?>;
 var ppcSalesArr = <?= json_encode($ppcSalesArr); ?>;
 var ppcAcosArr = <?= json_encode($acos); ?>;
+var dateArr = <?= json_encode($dateArr); ?>;
 
 var data = {
-		labels: ["day1", "day2", "day3", "day4", "day5", "day6", "day7", "day8", "day9", "day10", "day11", "day12", "day13", "day14", "day15", "day16", "day17", "day18", "day19", "day20", "day21", "day22", "day23", "day24", "day25", "day26", "day27", "day28", "day29", "day30", "day31", "day32", "day33", "day34", "day35", "day36", "day37", "day38", "day39", "day40", "day41", "day42", "day43", "day44", "day45", "day46", "day47", "day48", "day49", "day50", "day51", "day52", "day53", "day54", "day55", "day56", "day57", "day58", "day59", "day60"],
+		labels: dateArr,
 		datasets: [{
 			label: "Ad Spend",
 			data: adSpendArr,
