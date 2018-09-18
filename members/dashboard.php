62b4976a10b5e4c2c49705ccc38d51ee4511ecb3
Austin Tam <austin.tam7@gmail.com>
2018-09-18 15:17:38 -0400

    dashboard5 datepicker

--- members/dashboard.php
+++ members/dashboard.php
@@ -46,9 +46,6 @@ $_SESSION['active'] = intval($result[0]);
 
 <body class="">
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js"></script>
-  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-
 
   <div class="wrapper ">
 
--- members/includes/dashpages/main/dashboard_active5.php
+++ members/includes/dashpages/main/dashboard_active5.php
@@ -202,6 +202,7 @@ $dateArr = array_reverse($dateArr);
   </div>
 </div> -->
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js"></script>
 <div class="row">
 	<div class="col-lg-12 col-md-12 col-sm-12">
 		<div class="card ">
