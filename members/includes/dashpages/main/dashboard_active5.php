c6de37ee492de600da654132a3d8ed223af4421e
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-18 15:06:26 -0400

    asdf

--- members/includes/dashpages/main/dashboard_active5.php
+++ members/includes/dashpages/main/dashboard_active5.php
@@ -202,11 +202,8 @@ $dateArr = array_reverse($dateArr);
   </div>
 </div> -->
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+
+
 <div class="row">
 	<div class="col-lg-12 col-md-12 col-sm-12">
 		<div class="card ">
@@ -241,7 +238,7 @@ $dateArr = array_reverse($dateArr);
 
 <script type="text/javascript">
 $.noConflict();
-jQuery(function() {
+$(function() {
 
     var start = moment().subtract(29, 'days');
     var end = moment();
