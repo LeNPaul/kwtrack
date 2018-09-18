3116c53538af16a604dca45773e15ab47def9a41
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-18 14:53:22 -0400

    asdf

--- members/includes/dashpages/helper.inc.php
+++ members/includes/dashpages/helper.inc.php
@@ -160,7 +160,7 @@
         $numMaxKeywords = count($result);
 
         // Insert keywords into database
-        for ($x = 0; $x < $countMaxKeywords; $x++) {
+        for ($x = 0; $x < $numMaxKeywords; $x++) {
 
           // Get status and bid for each keyword
           $kw_id = $result[$x]['keywordId'];
