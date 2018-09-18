19a7dee2e4bc445fd1ec516a3c89f20eee9d561a
Austin Tam <austin.tam7@gmail.com>
2018-09-18 15:12:07 -0400

    dashboard5 datepicker

--- members/dashboard.php
+++ members/dashboard.php
@@ -789,8 +789,10 @@ $_SESSION['active'] = intval($result[0]);
   <script src="assets/js/paper-dashboard.min.js?v=2.0.1" type="text/javascript"></script>
   <!-- Paper Dashboard DEMO methods, don't include it in your project! -->
   <script src="assets/demo/demo.js"></script>
-
-    <script>
+  
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+  <script>
     $(document).ready(function() {
       // Javascript method's body can be found in assets/js/demos.js
       demo.initDashboardPageCharts();
