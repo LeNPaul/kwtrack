a3bc8ad0e8ccb88c06b0eea546314d287ddfe867
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-19 15:28:56 -0400

    asdf

--- members/includes/dashpages/main/import_data.php
+++ members/includes/dashpages/main/import_data.php
@@ -401,6 +401,10 @@ storeAdGroupArrays($pdo, $dbSales, $result, 'sales');
 $result = $client->listAdGroups();
 $result = json_decode($result['response'], true);
 
+echo '<pre>';
+var_dump($result);
+echo '</pre>';
+
 // Iterate through all ad groups and insert them into database
 
 $sql = "INSERT INTO ad_groups (amz_adgroup_id, ad_group_name, amz_campaign_id, status, user_id)
