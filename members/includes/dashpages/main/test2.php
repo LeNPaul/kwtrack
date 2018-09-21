a0674fd59d791486f4be3ddaaafad2656c617d92
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-21 15:40:50 -0400

    asdf

--- members/includes/dashpages/main/test2.php
+++ members/includes/dashpages/main/test2.php
@@ -5,7 +5,30 @@ require '../../../database/pdo.inc.php';
 require '../helper.inc.php';
 use PDO;
 
-shell_exec("php test3.php");
+$result = $client->requestReport(
+	"keywords",
+	array("reportDate"    => "20180814",
+				"campaignType"  => "sponsoredProducts",
+				"metrics"       => "adGroupId,campaignId,keywordId,keywordText,matchType,impressions,clicks,cost,campaignBudget,attributedUnitsOrdered1d,attributedSales1d",
+				"segment"				=> "query"
+	)
+);
+
+// Get the report id so we can use it to get the report
+$result = json_decode($result['response'], true);
+$reportId = $result['reportId'];
+
+sleep(10);
+
+// Get the report using the report id
+$result = $client->getReport($reportId);
+$result = json_decode($result['response'], true);
+
+echo '<pre>';
+var_dump($result);
+echo '</pre>';
+
+//shell_exec("php test3.php");
 
 /*
 $refreshToken = 'Atzr|IwEBID8Cr8D51I4XzWRU5wdohHUoGJRY1rempo6uwgk_niC5AgqZo_SVul0Nt8V5oU1j8P2T08oPjR8gLKSsWnJuAflfBzcMky0NzBoKcSIYH62WJ4I86G6t4jGxU7fitoLO79TJPFjCoHPXyjnvaNLxFaJPxOaW3t4fLBH9-1RGsAaEdrP0-r85iVNgG_pQE2HA7bl_ZMqWoJbXhww-YEsfMH6tBKXG0S0dMreLkEkdx75eABfzKwdDm9jokTL8YZjkqj1ELRFOwK6Pgv1PsYTvdI2Us1fTw-Bu1n_n4am_vlrK4ntseK_dqFHvrV4_h0aup1hoChA5KZD2ID3fG4e4be4iCRC66QdJxmjv_q_o8RxoZR_bG0vhlkU2rSYKnMnZOj7nkRS2Z6JoRPWRLw7nP8nEfHLRkCQnrOn2PHkrKX7MWTIWt1f-_rkr3ocfvgKfcixFvTc6XmNGg0IYbVidw0thS3-AgSpnGaG0O7Q-W9VZPFRFtas1PltUG69LL0ko2EOz6yW-RG9071MfpUMgre2_TUildA68rlcdikXtNfMtyYNwqvQhlSZ_eVXWGclIpk4XQ39a-5eJiB8HVfsAvgdF';
