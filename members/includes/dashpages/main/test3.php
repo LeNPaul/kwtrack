7ae537815c77c87ed223c778f005f8741ec43c24
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-21 15:20:03 -0400

    asdf

--- members/includes/dashpages/main/test3.php
+++ members/includes/dashpages/main/test3.php
@@ -1,11 +1,6 @@
 #!/usr/bin/env php
 <?php
-namespace AmazonAdvertisingApi;
-require_once '../../AmazonAdvertisingApi/Client.php';
 require '../../../database/pdo.inc.php';
-require '../helper.inc.php';
-use PDO;
-
 
 $sql = "INSERT INTO keywords (kw_id) VALUES (:kw_id)";
 $stmt = $pdo->prepare($sql);
