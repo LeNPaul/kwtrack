611a030bad1c708c5bdabc0ad0e000f33653a20a
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-21 15:42:14 -0400

    asdf

--- members/includes/dashpages/main/test2.php
+++ members/includes/dashpages/main/test2.php
@@ -5,6 +5,21 @@ require '../../../database/pdo.inc.php';
 require '../helper.inc.php';
 use PDO;
 
+$refreshToken = 'Atzr|IwEBID8Cr8D51I4XzWRU5wdohHUoGJRY1rempo6uwgk_niC5AgqZo_SVul0Nt8V5oU1j8P2T08oPjR8gLKSsWnJuAflfBzcMky0NzBoKcSIYH62WJ4I86G6t4jGxU7fitoLO79TJPFjCoHPXyjnvaNLxFaJPxOaW3t4fLBH9-1RGsAaEdrP0-r85iVNgG_pQE2HA7bl_ZMqWoJbXhww-YEsfMH6tBKXG0S0dMreLkEkdx75eABfzKwdDm9jokTL8YZjkqj1ELRFOwK6Pgv1PsYTvdI2Us1fTw-Bu1n_n4am_vlrK4ntseK_dqFHvrV4_h0aup1hoChA5KZD2ID3fG4e4be4iCRC66QdJxmjv_q_o8RxoZR_bG0vhlkU2rSYKnMnZOj7nkRS2Z6JoRPWRLw7nP8nEfHLRkCQnrOn2PHkrKX7MWTIWt1f-_rkr3ocfvgKfcixFvTc6XmNGg0IYbVidw0thS3-AgSpnGaG0O7Q-W9VZPFRFtas1PltUG69LL0ko2EOz6yW-RG9071MfpUMgre2_TUildA68rlcdikXtNfMtyYNwqvQhlSZ_eVXWGclIpk4XQ39a-5eJiB8HVfsAvgdF';
+$profileId = '1215041354659387';
+
+
+// Instantiate client for advertising API
+$config = array(
+	"clientId" => "amzn1.application-oa2-client.4246e0f086e441259742c758f63ca0bf",
+	"clientSecret" => "9c9e07b214926479e14a0781051ecc3ad9b29686d3cef24e15eb130a47cabeb3",
+	"refreshToken" => $refreshToken,
+	"region" => "na",
+	"sandbox" => false,
+);
+$client = new Client($config);
+$client->profileId = $profileId;
+
 $result = $client->requestReport(
 	"keywords",
 	array("reportDate"    => "20180814",
