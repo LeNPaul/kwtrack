84d7efa04730aa9af213c96293aa4883ee9a932a
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-21 16:18:23 -0400

    asdf

--- members/includes/dashpages/cmanager/campaign_manager.php
+++ members/includes/dashpages/cmanager/campaign_manager.php
@@ -4,9 +4,6 @@
  *    Allows users to edit and change all their campaigns in PPCOLOGY.
  */
 
-// Check if user is not logged in. Redirect to login page if not logged in.
-checkLoggedIn();
-
 ?>
 
 <h2>Campaign Manager</h2>
