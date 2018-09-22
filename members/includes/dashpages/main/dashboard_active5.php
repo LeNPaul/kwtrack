9866c8fab573ee2820b265eadd8415a4919b26e8
Austin Tam <austin.tam7@gmail.com>
2018-09-21 23:01:05 -0400

    update chart

--- members/includes/dashpages/main/dashboard_active5.php
+++ members/includes/dashpages/main/dashboard_active5.php
@@ -273,7 +273,7 @@ $(function() {
            'Today': [moment(), moment()],
            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-		   'Last 60 days': [moment().subtract(58, 'days'), moment()],
+		   'Last 60 days': [moment().subtract(59, 'days'), moment()],
            'This Month': [moment().startOf('month'), moment().endOf('month')],
            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
         }
