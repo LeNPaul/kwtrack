bc62079a98973af82eea5e74f320ecaf349a7d6d
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-21 21:11:15 -0400

    asdf

--- members/includes/dashpages/main/dashboard_active5.php
+++ members/includes/dashpages/main/dashboard_active5.php
@@ -254,69 +254,6 @@ $dateArr = array_reverse($dateArr);
 
 $(function() {
 
-	Chart.defaults.global = {
-
-	    // Boolean - Determines whether to draw tooltips on the canvas or not
-	    showTooltips: true,
-
-	    // Array - Array of string names to attach tooltip events
-	    tooltipEvents: ["mousemove", "touchstart", "touchmove"],
-
-	    // String - Tooltip background colour
-	    tooltipFillColor: "rgba(0,0,0,0.8)",
-
-	    // String - Tooltip label font declaration for the scale label
-	    tooltipFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-
-	    // Number - Tooltip label font size in pixels
-	    tooltipFontSize: 14,
-
-	    // String - Tooltip font weight style
-	    tooltipFontStyle: "normal",
-
-	    // String - Tooltip label font colour
-	    tooltipFontColor: "#fff",
-
-	    // String - Tooltip title font declaration for the scale label
-	    tooltipTitleFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-
-	    // Number - Tooltip title font size in pixels
-	    tooltipTitleFontSize: 14,
-
-	    // String - Tooltip title font weight style
-	    tooltipTitleFontStyle: "bold",
-
-	    // String - Tooltip title font colour
-	    tooltipTitleFontColor: "#fff",
-
-	    // Number - pixel width of padding around tooltip text
-	    tooltipYPadding: 6,
-
-	    // Number - pixel width of padding around tooltip text
-	    tooltipXPadding: 6,
-
-	    // Number - Size of the caret on the tooltip
-	    tooltipCaretSize: 8,
-
-	    // Number - Pixel radius of the tooltip border
-	    tooltipCornerRadius: 6,
-
-	    // Number - Pixel offset from point x to tooltip edge
-	    tooltipXOffset: 10,
-
-	    // String - Template string for single tooltips
-	    tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>",
-
-	    // String - Template string for single tooltips
-	    multiTooltipTemplate: "<%= value %>",
-
-	    // Function - Will fire on animation progression.
-	    onAnimationProgress: function(){},
-
-	    // Function - Will fire on animation completion.
-	    onAnimationComplete: function(){}
-	}
-
     var start = moment().subtract(59, 'days');
     var end = moment();
 
@@ -416,7 +353,12 @@ var myChart = new Chart(ctx, {
 								position: 'left',
 								gridLines: {
 	                color: "rgba(0, 0, 0, 0)",
-		            }
+		            },
+								ticks: {
+									callback: function(value, index, values) {
+                  	return '$' + value;
+                  }
+								}
 								//stacked: true
 	            }, {
 			      		id: 'B',
@@ -428,7 +370,10 @@ var myChart = new Chart(ctx, {
 								ticks: {
 									beginAtZero: true,
 									display: false,
-									max: Math.round(Math.max.apply(null, ppcSalesArr)) * 2
+									max: Math.round(Math.max.apply(null, ppcSalesArr)) * 2,
+									callback: function(value, index, values) {
+                  	return value + '%';
+                  }
 								}
 							}]
     }
