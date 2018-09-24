4cec245d0b97c4143e9761410c30610de5dd6c97
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-24 12:31:45 -0400

    Dashbaord chart frontend

--- members/includes/dashpages/main/dashboard_active5.php
+++ members/includes/dashpages/main/dashboard_active5.php
@@ -416,6 +416,33 @@ var myChart = new Chart(ctx, {
 	}
 });
 
+Chart.plugins.register ( {
+      afterDatasetsDraw: function(chart) {
+        chart_type = chart.config.type;
+        if (chart.tooltip._active && chart.tooltip._active.length && chart_type === 'line') {
+             var activePoint = chart.tooltip._active[0],
+             ctx = chart.chart.ctx,
+             x_axis = chart.scales['x-axis-0'],
+            y_axis = chart.scales['y-axis-0'],
+            x = activePoint.tooltipPosition().x,
+            topY = y_axis.top,
+            bottomY = y_axis.bottom;
+
+
+       // draw line
+       ctx.save();
+       ctx.beginPath();
+       ctx.moveTo(x, topY+7);
+       ctx.lineTo(x, bottomY+1);
+       ctx.setLineDash([2,3]);
+       ctx.lineWidth = 1;
+       ctx.strokeStyle = '#ff7e01';
+       ctx.stroke();
+       ctx.restore();
+     }
+  }
+});
+
 function chartUpdate(startUpdate, endUpdate) {
 	startArr = dateArr.indexOf(startUpdate);
 	endArr = dateArr.indexOf(endUpdate);
