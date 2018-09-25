042811225b14e6bccb137eec8ad89dfe84a1f17d
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-25 16:55:16 -0400

    campaign manager backend

--- members/includes/dashpages/cmanager/campaign_manager.php
+++ members/includes/dashpages/cmanager/campaign_manager.php
@@ -40,7 +40,7 @@ $campaignData = cmGetCampaignData($pdo, $user_id);
       </tbody>
 
     </table> -->
-    <table id="campaign_manager" class="table table-hover table-light table-bordered" style="width:100%;"></table>
+    <table id="campaign_manager" class="table display nowrap" style="width:100%;"></table>
   </div>
 </div>
 
