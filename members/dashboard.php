bc1493cead320ef1d9e3dfc3a4f6c28ffa30e5a5
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-25 16:40:11 -0400

    campaign manager backend

--- members/dashboard.php
+++ members/dashboard.php
@@ -777,7 +777,7 @@ $_SESSION['active'] = intval($result[0]);
 
   <!-- <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script> -->
   <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
-  
+  <script src="assets/js/plugins/jquery.dataTables.min.js"></script>
 
   <!--	Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
   <script src="assets/js/plugins/bootstrap-tagsinput.js"></script>
