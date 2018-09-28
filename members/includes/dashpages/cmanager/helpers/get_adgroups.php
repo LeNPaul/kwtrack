87e19dd42379cdfd3f80b969b78e9387e477ce9d
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-28 12:46:45 -0400

    asdf

--- members/includes/dashpages/cmanager/helpers/get_adgroups.php
+++ members/includes/dashpages/cmanager/helpers/get_adgroups.php
@@ -6,7 +6,7 @@ $campaignName     = htmlspecialchars($_POST['campaignName']);
 $campaignDataBack = $_POST['campaignDataBack'];
 $campaignId       = $campaignDataBack[$campaignName];
 
-print_r(mGetAdGroupData($pdo, $campaignId);
+print_r(mGetAdGroupData($pdo, $campaignId));
 
 //return cmGetAdGroupData($pdo, $campaignId);
 ?>
