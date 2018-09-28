a2a7eff1d5c9ac2738db7e6643ea2dbe6ba65382
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-09-28 12:22:15 -0400

    asdf

--- members/includes/dashpages/cmanager/helpers/get_adgroups.php
+++ members/includes/dashpages/cmanager/helpers/get_adgroups.php
@@ -2,7 +2,7 @@
 include '../cm_helper.inc.php';
 include '../../../../database/pdo.inc.php';
 
-$campaignName     = $_POST['campaignName'];
+$campaignName     = htmlspecialchars($_POST['campaignName']);
 $campaignDataBack = $_POST['campaignDataBack'];
 $campaignId       = $campaignDataBack[$campaignName];
 
