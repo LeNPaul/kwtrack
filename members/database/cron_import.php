7c00b4934fac6841da0985e001ecbc5c9ed05f25
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-10-03 18:22:25 -0400

    campaign manager backend

---
+++ members/database/cron_import.php
@@ -0,0 +1,13 @@
+<?php
+include 'pdo.inc.php';
+
+// query all userIDs from ppc_keywords where active = 5 and store in array (userIDs)
+$userIDs = [];
+$sql = "SELECT user_id FROM users WHERE active=5";
+$stmt = $pdo->query($sql);
+$userIDs = $stmt->execute(PDO::FETCH_COLUMN);
+
+echo '<pre>';
+var_dump($userIDs);
+echo '</pre>';
+?>
