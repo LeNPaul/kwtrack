3d88710635b1e1467ad3a6cfe0c65f0cf6855ccc
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-10-03 19:40:09 -0400

    campaign manager backend

--- members/database/cron_import.php
+++ members/database/cron_import.php
@@ -1,6 +1,26 @@
 <?php
 include 'pdo.inc.php';
 
+/*
+
+██   ██ ███████ ██      ██████  ███████ ██████  ███████
+██   ██ ██      ██      ██   ██ ██      ██   ██ ██
+███████ █████   ██      ██████  █████   ██████  ███████
+██   ██ ██      ██      ██      ██      ██   ██      ██
+██   ██ ███████ ███████ ██      ███████ ██   ██ ███████
+
+*/
+
+/*
+ *  function array_search2D(Array $array, Mixed $key, Mixed $value) --> Array
+ *    --> Returns index where $key => $value pair is found in a 2D array formatted like:
+ *          [ i => [key1 => value1, key2 => value2, keyN => valueN] ]
+ */
+
+function array_search2D($array, $key, $value) {
+  return (array_search($value, array_column($array, $key)););
+}
+
 
 /*
 
@@ -14,14 +34,16 @@ include 'pdo.inc.php';
 
 // query all userIDs from ppc_keywords where active = 5 and store in array (userIDs)
 $userIDs   = [];
-$reportIDs = [];
+// $reportIDs = [];
 
 $sql     = "SELECT user_id, profileId, refresh_token FROM users WHERE active=5";
 $stmt    = $pdo->query($sql);
 $userIDs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// for each userID, request report for yesterday's keywords and from report, store all keyword IDs (reportKeywordID)
+// for each userID, request report for yesterday's keywords and from report, store all keyword IDs (reportKeywordIDs)
+
 for ($i = 0; $i < count($userIDs); $i++) {
+  $user_id = $userIDs[$i]['user_id'];
   // Instantiate client for advertising API
   $config = array(
   	"clientId" => "amzn1.application-oa2-client.4246e0f086e441259742c758f63ca0bf",
@@ -42,15 +64,59 @@ for ($i = 0; $i < count($userIDs); $i++) {
     )
   );
 
+  $code = $result['code'];
+
   // Get the report id so we can use it to get the report
-  $result      = json_decode($result['response'], true);
-  $reportId    = $result['reportId'];
-  $reportIDs[] = $reportId;
+  $result2         = json_decode($result['response'], true);
+  $reportId        = $result2['reportId'];
+  // $reportIDs[]     = $reportId;
+
+  // Keep pinging the report until we get a 200 code
+  while ($code == 202) {
+    $result = $client->getReport($reportId);
+    $code   = $result['code'];
+  }
+
+  // Once the code is NOT 202, we know that we have a valid report. Store it in $result.
+  $result = json_decode($result['response'], true);
+
+  // Store all keyword IDs from the report in reportKeywordIDs
+  $reportKeywordIDs = [];
+  for ($j = 0; $j < count($result); $j++) {
+    $reportKeywordIDs[] = $result[$i]['keywordId'];
+  }
+
+  // Fetch and store amz_kw_id for the current user in array (dbKeywordIDs)
+  $sql          = "SELECT amz_kw_id FROM ppc_keywords WHERE user_id={$user_id}";
+  $stmt         = $pdo->query($sql);
+  $dbKeywordIDs = $stmt->fetchAll(PDO::FETCH_COLUMN);
+
+  // First check if length of reportKeywordIDs > length of dbKeywordIDs
+  if (count($reportKeywordIDs) > count($dbKeywordIDs)) {
+    $diff = array_diff($reportKeywordIDs, $dbKeywordIDs);
+
+    // foreach diff, find index of extra keywords in the report.
+    for ($a = 0; $a < count($diff); $a++) {
+
+    }
+
+  } else {
+
+  }
 }
 
-// Iterate through $reportIDs and obtain keyword report
-for () {
+/*
+// Iterate through $reportIDs and obtain keyword report. Store in Array kwReports
+$currentReportID = '';
+$code = 202;
+for ($j = 0; $j < count($reportIDs); $j++) {
+  // Keep trying to request the report until we get a "200" code response
+  while ($code == 202) {
+    $result = $client->getReport($reportIDs[$i]);
+    $code   = $result['code'];
+  }
 
 }
+*/
 
 ?>
--- members/includes/dashpages/main/test2.php
+++ members/includes/dashpages/main/test2.php
@@ -5,6 +5,10 @@ require '../../../database/pdo.inc.php';
 require '../helper.inc.php';
 use PDO;
 
+function array_search2D($array, $key, $value) {
+  return (array_search($value, array_column($array, $key)););
+}
+
 $refreshToken = 'Atzr|IwEBID8Cr8D51I4XzWRU5wdohHUoGJRY1rempo6uwgk_niC5AgqZo_SVul0Nt8V5oU1j8P2T08oPjR8gLKSsWnJuAflfBzcMky0NzBoKcSIYH62WJ4I86G6t4jGxU7fitoLO79TJPFjCoHPXyjnvaNLxFaJPxOaW3t4fLBH9-1RGsAaEdrP0-r85iVNgG_pQE2HA7bl_ZMqWoJbXhww-YEsfMH6tBKXG0S0dMreLkEkdx75eABfzKwdDm9jokTL8YZjkqj1ELRFOwK6Pgv1PsYTvdI2Us1fTw-Bu1n_n4am_vlrK4ntseK_dqFHvrV4_h0aup1hoChA5KZD2ID3fG4e4be4iCRC66QdJxmjv_q_o8RxoZR_bG0vhlkU2rSYKnMnZOj7nkRS2Z6JoRPWRLw7nP8nEfHLRkCQnrOn2PHkrKX7MWTIWt1f-_rkr3ocfvgKfcixFvTc6XmNGg0IYbVidw0thS3-AgSpnGaG0O7Q-W9VZPFRFtas1PltUG69LL0ko2EOz6yW-RG9071MfpUMgre2_TUildA68rlcdikXtNfMtyYNwqvQhlSZ_eVXWGclIpk4XQ39a-5eJiB8HVfsAvgdF';
 $profileId = '1215041354659387';
 
@@ -30,10 +34,6 @@ $result = $client->requestReport(
 	)
 );
 
-echo '<pre>';
-var_dump($result);
-echo '</pre>';
-
 // Get the report id so we can use it to get the report
 $result = json_decode($result['response'], true);
 $reportId = $result['reportId'];
@@ -42,14 +42,18 @@ sleep(10);
 
 // Get the report using the report id
 $result = $client->getReport($reportId);
-echo '<pre>';
-var_dump($result);
-echo '</pre>';
-
 $result = json_decode($result['response'], true);
 
 echo '<pre>';
 var_dump($result);
 echo '</pre>';
+
+$index = array_search2D($result, 'keywordId', 253505656879259);
+echo $index . '<br /><br />';
+
+echo '<pre>';
+var_dump($result[$index]);
+echo '</pre>';
+
 
 ?>
