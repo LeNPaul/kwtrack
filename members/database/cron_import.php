19811add70dc49a3e254b2855174c52430cce7c5
Fernand Gonzales <fernandgonzales@Fernands-MacBook-Pro.local>
2018-10-03 18:36:35 -0400

    campaign manager backend

--- members/database/cron_import.php
+++ members/database/cron_import.php
@@ -1,30 +1,56 @@
 <?php
 include 'pdo.inc.php';
 
+
+/*
+
+██   ██ ███████ ██    ██ ██     ██  ██████  ██████  ██████  ███████
+██  ██  ██       ██  ██  ██     ██ ██    ██ ██   ██ ██   ██ ██
+█████   █████     ████   ██  █  ██ ██    ██ ██████  ██   ██ ███████
+██  ██  ██         ██    ██ ███ ██ ██    ██ ██   ██ ██   ██      ██
+██   ██ ███████    ██     ███ ███   ██████  ██   ██ ██████  ███████
+
+*/
+
 // query all userIDs from ppc_keywords where active = 5 and store in array (userIDs)
-$userIDs = [];
-$sql = "SELECT user_id, profileId, refresh_token FROM users WHERE active=5";
-$stmt = $pdo->query($sql);
+$userIDs   = [];
+$reportIDs = [];
+
+$sql     = "SELECT user_id, profileId, refresh_token FROM users WHERE active=5";
+$stmt    = $pdo->query($sql);
 $userIDs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo '<pre>';
-var_dump($userIDs);
-echo '</pre>';
-
 // for each userID, request report for yesterday's keywords and from report, store all keyword IDs (reportKeywordID)
-/*
 for ($i = 0; $i < count($userIDs); $i++) {
   // Instantiate client for advertising API
   $config = array(
   	"clientId" => "amzn1.application-oa2-client.4246e0f086e441259742c758f63ca0bf",
   	"clientSecret" => "9c9e07b214926479e14a0781051ecc3ad9b29686d3cef24e15eb130a47cabeb3",
-  	"refreshToken" => $refreshToken,
+  	"refreshToken" => $userIDs[$i]['refresh_token'],
   	"region" => "na",
   	"sandbox" => false,
   );
   $client = new Client($config);
-  $client->profileId = $profileId;
+  $client->profileId = $userIDs[$i]['profileId'];
+
+  // Request report, then get report ID and append to reportIDs
+  $result = $client->requestReport(
+    "keywords",
+    array("reportDate"    => $date,
+          "campaignType"  => "sponsoredProducts",
+          "metrics"       => "adGroupId,campaignId,keywordId,keywordText,matchType,impressions,clicks,cost,campaignBudget,attributedUnitsOrdered1d,attributedSales1d"
+    )
+  );
+
+  // Get the report id so we can use it to get the report
+  $result      = json_decode($result['response'], true);
+  $reportId    = $result['reportId'];
+  $reportIDs[] = $reportId;
 }
 
-*/
+// Iterate through $reportIDs and obtain keyword report
+for () {
+
+}
+
 ?>
--- members/includes/dashpages/main/test2.php
+++ members/includes/dashpages/main/test2.php
@@ -20,7 +20,7 @@ $config = array(
 $client = new Client($config);
 $client->profileId = $profileId;
 
-/*
+
 $result = $client->requestReport(
 	"keywords",
 	array("reportDate"    => "20180907",
@@ -34,8 +34,6 @@ $result = $client->requestReport(
 $result = json_decode($result['response'], true);
 $reportId = $result['reportId'];
 
-sleep(10);
-
 // Get the report using the report id
 $result = $client->getReport($reportId);
 $result = json_decode($result['response'], true);
@@ -43,124 +41,5 @@ $result = json_decode($result['response'], true);
 echo '<pre>';
 var_dump($result);
 echo '</pre>';
-*/
 
-$result = $client->requestSnapshot(
-    "campaigns",
-    array("stateFilter" => "enabled,paused,archived",
-          "campaignType" => "sponsoredProducts"));
-$result = json_decode($result['response'], true);
-$snapshotId = $result['snapshotId'];
-echo '<pre>';
-var_dump($result);
-echo '</pre>';
-// sleep(10);
-// $result = $client->getSnapshot($snapshotId);
-// $result = json_decode($result['response'], true);
-$result = $client->listCampaigns();
-$result = json_decode($result['response'], true);
-echo '<pre>';
-var_dump($result);
-echo '</pre>';
-
-//shell_exec("php test3.php");
-
-/*
-$refreshToken = 'Atzr|IwEBID8Cr8D51I4XzWRU5wdohHUoGJRY1rempo6uwgk_niC5AgqZo_SVul0Nt8V5oU1j8P2T08oPjR8gLKSsWnJuAflfBzcMky0NzBoKcSIYH62WJ4I86G6t4jGxU7fitoLO79TJPFjCoHPXyjnvaNLxFaJPxOaW3t4fLBH9-1RGsAaEdrP0-r85iVNgG_pQE2HA7bl_ZMqWoJbXhww-YEsfMH6tBKXG0S0dMreLkEkdx75eABfzKwdDm9jokTL8YZjkqj1ELRFOwK6Pgv1PsYTvdI2Us1fTw-Bu1n_n4am_vlrK4ntseK_dqFHvrV4_h0aup1hoChA5KZD2ID3fG4e4be4iCRC66QdJxmjv_q_o8RxoZR_bG0vhlkU2rSYKnMnZOj7nkRS2Z6JoRPWRLw7nP8nEfHLRkCQnrOn2PHkrKX7MWTIWt1f-_rkr3ocfvgKfcixFvTc6XmNGg0IYbVidw0thS3-AgSpnGaG0O7Q-W9VZPFRFtas1PltUG69LL0ko2EOz6yW-RG9071MfpUMgre2_TUildA68rlcdikXtNfMtyYNwqvQhlSZ_eVXWGclIpk4XQ39a-5eJiB8HVfsAvgdF';
-$profileId = '1215041354659387';
-$user_id = 2;
-
-// Instantiate client for advertising API
-$config = array(
-	"clientId" => "amzn1.application-oa2-client.4246e0f086e441259742c758f63ca0bf",
-	"clientSecret" => "9c9e07b214926479e14a0781051ecc3ad9b29686d3cef24e15eb130a47cabeb3",
-	"refreshToken" => $refreshToken,
-	"region" => "na",
-	"sandbox" => false,
-);
-$client = new Client($config);
-$client->profileId = $profileId;
-
-// $status = $client->getBiddableKeywordEx('225434316448793');
-// $status = json_decode($status['response'], true);
-//
-// echo '<pre>';
-// var_dump($status);
-// echo '</pre>';
-
-// $sql = "SELECT sales FROM campaigns WHERE user_id=2";
-// $stmt = $pdo->query($sql);
-// $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-//
-// echo '<pre>';
-// var_dump($result);
-// echo '</pre>';
-
-/*
-$result = $client->requestReport(
-  "keywords",
-  array("reportDate"    => '20180725',
-        "campaignType"  => "sponsoredProducts",
-        //"metrics"       => "adGroupId,campaignId,keywordId,keywordText,matchType,impressions,clicks,cost,campaignBudget,attributedUnitsOrdered1d,attributedSales1d"
-        "metrics"       => "impressions,clicks,cost,attributedUnitsOrdered1d,attributedSales1d"
-  )
-);
-
-// Get the report id so we can use it to get the report
-$result = json_decode($result['response'], true);
-
-echo '<pre>';
-var_dump($result);
-echo '<pre>';
-
-$reportId = $result['reportId'];
-
-sleep(10);
-
-// Get the report using the report id
-$result = $client->getReport($reportId);
-$result = json_decode($result['response'], true);
-
-echo '<pre>';
-var_dump($result);
-echo '<pre>';
-
-
-$result = $client->listAdGroups();
-echo '<pre>';
-var_dump(json_decode($result['response'], true));
-echo '</pre>';
-*/
-/*
-
-$arr = array(
-	'id1' => [1,2,3,4,5,6,7,8,9,10],
-	'id9' => [10],
-	'id9' => [10]
-);
-
-//$arr = adjustDayOffset($arr, 10);
-
-foreach ($arr as $key => $value) {
-	echo $key . ' => ';
-	print_r($value);
-	echo ' - '.count($value) . '<br />';
-
-	if (count($value) < 10) {
-		echo 'starting<br />';
-		// Prepend $numDays-1 0's to $value
-		for ($i = 1; $i < 10; $i++) {
-			$value[] = 0;
-		}
-	}
-
-	echo '<br /><br />'.$key . ' => ';
-	print_r($value);
-	echo ' - '.count($value) . '<br />';
-}
-
-// echo '<pre>';
-// var_dump($arr);
-// echo '</pre>';
-*/
 ?>
