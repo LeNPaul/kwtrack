1e068d32782dcb8396578e4c786236a66fb6e431
Austin Tam <austin.tam7@gmail.com>
2018-10-13 15:03:38 -0400

    chart

--- members/includes/dashpages/main/assets/dashboard_chart.php
+++ members/includes/dashpages/main/assets/dashboard_chart.php
@@ -226,4 +226,28 @@
 
     myChart.update();
   };
+  
+  Chart.defaults.LineWithLine = Chart.defaults.line;
+  Chart.controllers.LineWithLine= Chart.controllers.line.extend({
+	 draw: function(ease) {
+		Chart.controllers.line.prototype.draw.call(this, ease);
+		
+		if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
+			var activePoint = this.chart.tooltip._active[0],
+				ctx = thix.chart.ctx,
+				x = activePoint.tooltipPosition().x,
+				topY = this.chart.scales['y-axis-0'].top,
+				bottomY = this.chart.scales['y-axis-0'].bottom;
+				
+			ctx.save();
+			ctx.beginPath();
+			ctx.moveTo(x, topY);
+			ctx.lineTo(x, bottomY);
+			ctx.lineWidth = 2;
+			ctx.strokeStyle = '#07C';
+			ctx.stroke();
+			ctx.restore();
+		}
+	 }	 
+  });
 </script>
