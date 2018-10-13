21fa8aa585a7aa83eeff26fa1a1f1f11f325f95b
Austin Tam <austin.tam7@gmail.com>
2018-10-13 17:18:16 -0400

    chart

--- members/includes/dashpages/main/dashboard_active5.php
+++ members/includes/dashpages/main/dashboard_active5.php
@@ -270,9 +270,9 @@ $dateArr = array_reverse($dateArr);
 			</div>
 			<br>
 
-			<div class="chartWrapper" width="1000" height="400">
-				<canvas id="cursor" width="1000" height="400" style="top:0; left:0; position: relative"></canvas>
-				<canvas id="lineChart" width="1000" height="400" style="position: relative; top:0; left:0;"></canvas>
+			<div class="chartWrapper" width="1000" height="400" style="position: relative">
+				<canvas id="cursor" width="1000" height="400" style="top:0; left:0; position: absolute"></canvas>
+				<canvas id="lineChart" width="1000" height="400" style="position: absolute; top:0; left:0;"></canvas>
 			</div>
 	  </div>
 
