d264617e435e37a55d1e6c4d10c9153b07144042
Fernand <fernandgee@gmail.com>
2018-10-15 16:08:25 -0600

    import algo revisions

--- members/includes/dashpages/helper.inc.php
+++ members/includes/dashpages/helper.inc.php
@@ -291,7 +291,7 @@
             $status = $result2['status'];
           } else {
             $status = 'DONE';
-            if (strlen($result['response']) < 1000) { var_dump($result); }
+            var_dump($result);
           }
 
           echo $status . ' in loop <br />';
