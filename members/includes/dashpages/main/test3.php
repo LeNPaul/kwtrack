1b4be7de38ccc2c2ae76a2861b7445f786dfa1ed
Fernand <fernandgee@gmail.com>
2018-10-16 15:51:56 -0600

    import algo debugging

--- members/includes/dashpages/main/test3.php
+++ members/includes/dashpages/main/test3.php
@@ -2,20 +2,22 @@
 <?php
 require '../../../database/pdo.inc.php';
 
-function diff($b, $a) {
-  $at = array_flip($a);
-  $d = array();
-  foreach ($b as $i)
-    if (!isset($at[$i]))
-      $d[] = $i;
-  return $d;
+class Arrays {
+  public function diff($b, $a) {
+    $at = array_flip($a);
+    $d = array();
+    foreach ($b as $i)
+      if (!isset($at[$i]))
+        $d[] = $i;
+    return $d;
+  }
 }
 
 $arr1 = [1,2,3,4,5,6,7,8,9,10];
 $arr2 = [2,3,4,5,11];
 
 echo '<pre>';
-var_dump(diff($arr1, $arr2));
+var_dump(Arrays->diff($arr1, $arr2));
 echo '</pre>';
 
 ?>
