5a3232b48f81ba1ec7290a0a4a3b85ae73a8fda8
Austin Tam <austin.tam7@gmail.com>
2018-10-17 19:27:51 -0400

    chart

--- members/includes/dashpages/main/dashboard_active5.php
+++ members/includes/dashpages/main/dashboard_active5.php
@@ -301,7 +301,7 @@ $dateArr = array_reverse($dateArr);
 	  </div> -->
 
       <div class="card-body">
-			<div id="reportrange" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; width: 30%">
+			<div id="reportrange" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; width: 33%">
 				<i class="fa fa-calendar"></i>
 				<span></span> <i class="fa fa-caret-down"></i>
 			</div>
