6b9e87a716d1292dca9a42a6984ec836031bc7bb
Fernand <fernandgee@gmail.com>
2018-10-18 15:58:21 -0600

    import algo revisions

--- members/includes/dashpages/main/test2.php
+++ members/includes/dashpages/main/test2.php
@@ -14,6 +14,21 @@ function array_search2D($array, $key, $value) {
 $refreshToken = 'Atzr|IwEBID8Cr8D51I4XzWRU5wdohHUoGJRY1rempo6uwgk_niC5AgqZo_SVul0Nt8V5oU1j8P2T08oPjR8gLKSsWnJuAflfBzcMky0NzBoKcSIYH62WJ4I86G6t4jGxU7fitoLO79TJPFjCoHPXyjnvaNLxFaJPxOaW3t4fLBH9-1RGsAaEdrP0-r85iVNgG_pQE2HA7bl_ZMqWoJbXhww-YEsfMH6tBKXG0S0dMreLkEkdx75eABfzKwdDm9jokTL8YZjkqj1ELRFOwK6Pgv1PsYTvdI2Us1fTw-Bu1n_n4am_vlrK4ntseK_dqFHvrV4_h0aup1hoChA5KZD2ID3fG4e4be4iCRC66QdJxmjv_q_o8RxoZR_bG0vhlkU2rSYKnMnZOj7nkRS2Z6JoRPWRLw7nP8nEfHLRkCQnrOn2PHkrKX7MWTIWt1f-_rkr3ocfvgKfcixFvTc6XmNGg0IYbVidw0thS3-AgSpnGaG0O7Q-W9VZPFRFtas1PltUG69LL0ko2EOz6yW-RG9071MfpUMgre2_TUildA68rlcdikXtNfMtyYNwqvQhlSZ_eVXWGclIpk4XQ39a-5eJiB8HVfsAvgdF';
 $profileId = '1215041354659387';
 
+
+function getSnapshot($client, $snapshotId) {
+  do {
+    $report = $client->getSnapshot($snapshotId);
+    $result2 = json_decode($report['response'], true);
+    if (array_key_exists('status', $result2)) {
+      $status = $result2['status'];
+    } else {
+      $status = 'DONE';
+      $report = $result2;
+    }
+  } while ($status == 'IN_PROGRESS');
+  return $report;
+}
+
 // Instantiate client for advertising API
 $config = array(
 	"clientId" => "amzn1.application-oa2-client.4246e0f086e441259742c758f63ca0bf",
@@ -29,6 +44,15 @@ $kwSnapshot = $client->requestSnapshot(
   "keywords",
   array("stateFilter"  => "enabled,paused,archived",
         "campaignType" => "sponsoredProducts"));
+
+echo '<pre>';
+var_dump($kwSnapshot);
+echo '</pre>';
+
+$snapshotId = json_decode($kwSnapshot, true);
+$snapshotId = $snapshotId['snapshotId'];
+
+$kwSnapshot = getSnapshot($snapshotId);
 
 echo '<pre>';
 var_dump($kwSnapshot);
