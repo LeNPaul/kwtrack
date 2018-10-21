60a1b2310d5ae07a44c2c55270dac0d7b8d136d5
Fernand <fernandgee@gmail.com>
2018-10-21 12:23:24 -0600

    campaign manager

--- members/includes/dashpages/cmanager/helpers/toggle_campaigns.php
+++ members/includes/dashpages/cmanager/helpers/toggle_campaigns.php
@@ -2,6 +2,7 @@
 namespace AmazonAdvertisingApi;
 require_once '../../../AmazonAdvertisingApi/Client.php';
 include '../../../../database/pdo.inc.php';
+use PDO;
 
 $user_id          = $_POST['user_id'];
 $toggle           = $_POST['toggle'];
@@ -9,8 +10,15 @@ $campaignName     = htmlspecialchars($_POST['campaignName']);
 $campaignDataBack = $_POST['cDataBack'];
 $campaignId       = $campaignDataBack[$campaignName];
 
-$sql = "SELECT refresh_token, profileId FROM users WHERE user_id=:user_id";
-z
+$stmt = $pdo->prepare("SELECT refresh_token, profileId FROM users WHERE user_id=?");
+$stmt->bindParam(1, $user_id);
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOCC);
+echo '<pre>';
+var_dump($result);
+echo '</pre>';
+
+
 if ($toggle === true) {
 
 } else {
--- members/includes/dashpages/main/test3.php
+++ members/includes/dashpages/main/test3.php
@@ -1,5 +1,13 @@
 #!/usr/bin/env php
 <?php
 require '../../../database/pdo.inc.php';
-echo dirname(__FILE__);
+
+$stmt = $pdo->prepare("SELECT refresh_token, profileId FROM users WHERE user_id=?");
+$stmt->bindParam(1, $user_id);
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOCC);
+echo '<pre>';
+var_dump($result);
+echo '</pre>';
+
 ?>
