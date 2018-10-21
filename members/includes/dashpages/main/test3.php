1bde2695652274d2642e400acf2396efe8dbfc5b
Fernand <fernandgee@gmail.com>
2018-10-21 12:27:08 -0600

    campaign manager

--- members/includes/dashpages/main/test3.php
+++ members/includes/dashpages/main/test3.php
@@ -5,7 +5,7 @@ require '../../../database/pdo.inc.php';
 $stmt = $pdo->prepare("SELECT refresh_token, profileId FROM users WHERE user_id=?");
 $stmt->bindParam(1, $user_id);
 $stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_ASSOCC);
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 echo '<pre>';
 var_dump($result);
 echo '</pre>';
