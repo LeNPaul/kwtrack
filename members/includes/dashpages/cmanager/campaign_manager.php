387ab936ab98e4375e4c9008e3082b2783117a33
Fernand <fernandgee@gmail.com>
2018-10-20 20:43:10 -0600

    campaign manager

--- members/includes/dashpages/cmanager/campaign_manager.php
+++ members/includes/dashpages/cmanager/campaign_manager.php
@@ -104,7 +104,7 @@ $(document).ready( function () {
 
       $(".toggle").on("click", function() {
         $(this).toggleClass('toggle-selected');
-        console.log(console.log( dt.rows('.selected').data()[1] );
+        console.log(dt.rows('.selected').data()[1]);
         $(this).toggleClass('toggle-selected');
 
         if ($(this).hasClass("off")) {
