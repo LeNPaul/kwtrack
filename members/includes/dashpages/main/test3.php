81b16b6137cebf320814600fb47adbcef4dfd615
Fernand <fernandgee@gmail.com>
2018-10-21 12:37:06 -0600

    campaign manager

--- members/includes/dashpages/main/test3.php
+++ members/includes/dashpages/main/test3.php
@@ -6,7 +6,7 @@ $user_id = 2;
 $stmt = $pdo->prepare("SELECT refresh_token, profileId FROM users WHERE user_id=?");
 $stmt->bindParam(1, $user_id);
 $stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_COLUMN);
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 echo '<pre>';
 var_dump($result);
 echo '</pre>';
