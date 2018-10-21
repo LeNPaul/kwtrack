985477dc858341f52232a3f940c72a59357d44eb
Fernand <fernandgee@gmail.com>
2018-10-21 14:16:29 -0600

    campaign manager

--- members/includes/dashpages/cmanager/cm_helper.inc.php
+++ members/includes/dashpages/cmanager/cm_helper.inc.php
@@ -83,7 +83,7 @@ function cmGetCampaignData($pdo, $user_id) {
     $units_sold  = ($units_sold == 0) ? '-' : $units_sold;
 
     $campaignLink = '<a href="javascript:void(0)" class="name c_link">' . $result[$i]['campaign_name'] . '</a>';
-    $budget =  '<div class="input-group input-group-sm">
+    $budget =  '<div class="input-group input-group-xs">
                   <div class="input-group-prepend">
                     <span class="input-group-text">$</span>
                   </div>
