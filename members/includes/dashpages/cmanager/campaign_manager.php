9673b24bf4008eaebb672c738338fa8109e21a7c
Fernand <fernandgee@gmail.com>
2018-10-21 13:52:54 -0600

    campaign manager

--- members/includes/dashpages/cmanager/campaign_manager.php
+++ members/includes/dashpages/cmanager/campaign_manager.php
@@ -100,7 +100,7 @@ $(document).ready( function () {
       $('.toggle-campaign').bootstrapToggle({
         on: '<i class="fa fa-play"></i>',
         off: "Paused",
-        size: "xs",
+        size: "sm",
         onstyle: "success",
         offstyle: "primary"
       });
