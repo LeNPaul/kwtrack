7f19235db16a05a495df79ee06718bf33bb7e3e6
Austin Tam <austin.tam7@gmail.com>
2018-10-24 22:39:44 -0400

    campaign manager

--- members/includes/dashpages/cmanager/campaign_manager.php
+++ members/includes/dashpages/cmanager/campaign_manager.php
@@ -56,7 +56,7 @@ for ($j = 1; $j < 60; $j++) {
 var dataTableFlag = 1;
 var currentCampaign = "";
 var adGroupName = "";
-var allCampaigns = <a href=\"javascript:void(0)\" class=\"name all_link\">All Campaigns</a>";
+var allCampaigns = "<a href=\"javascript:void(0)\" class=\"name all_link\">All Campaigns</a>";
 
 $(document).ready( function () {
   var dataset       = <?= json_encode($campaignDataFront) ?>;
