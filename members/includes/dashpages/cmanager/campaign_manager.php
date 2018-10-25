080e467c31e3832fa08cbaf8447f5d03d1f5feb2
Austin Tam <austin.tam7@gmail.com>
2018-10-25 12:43:47 -0400

    campaign manager

--- members/includes/dashpages/cmanager/campaign_manager.php
+++ members/includes/dashpages/cmanager/campaign_manager.php
@@ -103,8 +103,7 @@ $(document).ready( function () {
       
 	    	//breadcrumbs ALL CAMPAIGNS click
 	$(".all_link").on("click", function() {
-		dt.hide();
-		//dt.clear().rows.add(dataset).draw();
+		dt.clear().rows.add(dataset).draw();
 		console.log("all campaigns clicked");
 	});
 	  
