0da2d1dd4b8b19fbdfeddb02aa8e6bd9fc7c2121
Austin Tam <austin.tam7@gmail.com>
2018-11-06 13:26:32 -0500

    campaign manager

--- members/includes/dashpages/cmanager/campaign_manager.php
+++ members/includes/dashpages/cmanager/campaign_manager.php
@@ -298,24 +298,7 @@ $(document).ready( function () {
 
 	          $('td input').bootstrapToggle();
 
-	          
-
-	        } // drawCallback
-
-	      }; // adgrOptions
-
-	      dt.destroy(false);
-	      dt_adgroups = $("#adgroup_manager").DataTable(adgrOptions);
-	    }, // success (campaign manager)
-
-	    error: function(msg) {
-	      alert(msg);
-	    } //error (campaign manager)
-
-	  }); //ajax
-	}); //on campaign name click
-	
-	$(".ag_link").on("click", function() {
+	          $(".ag_link").on("click", function() {
 
 	            adgroupName     = $(this).text();
 
@@ -400,6 +383,21 @@ $(document).ready( function () {
 	            });
 
 	          }); // .ag_link on click
+
+	        } // drawCallback
+
+	      }; // adgrOptions
+
+	      dt.destroy(false);
+	      dt_adgroups = $("#adgroup_manager").DataTable(adgrOptions);
+	    }, // success (campaign manager)
+
+	    error: function(msg) {
+	      alert(msg);
+	    } //error (campaign manager)
+
+	  }); //ajax
+	}); //on campaign name click
 
   $('#campaign_manager tbody').on('click', 'tr', function() {
 	  $(this).toggleClass('selected');
