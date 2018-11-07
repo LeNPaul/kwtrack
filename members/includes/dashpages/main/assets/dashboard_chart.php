5eb04f1c0d71a28bc5b1e2652987d0bbb198be61
Fernand <fernandgee@gmail.com>
2018-11-06 19:10:23 -0700

    Dashboard chart: vertical line with snapping on the x-axis

--- members/includes/dashpages/main/assets/dashboard_chart.php
+++ members/includes/dashpages/main/assets/dashboard_chart.php
@@ -94,14 +94,22 @@
       hoverBorderWidth: 3,
       // backgroundColor: "rgba(89, 255, 152, 0.1)",
       borderWidth: 2.5,
-      borderColor: "rgba(89, 255, 152, 0.7)",
+      borderColor: "rgba(89, 255, 152, 0.7)"
       //type: 'line'
     }, {
       label: "PPC ACoS (%)",
       yAxisID: 'B',
       data: ppcAcosArr,
-      fill: true,
-      backgroundColor: "rgba(114, 187, 255, 0.4)",
+      fill: false,
+      //backgroundColor: "rgba(114, 187, 255, 0.4)",
+      pointRadius: 0,
+      hoverRadius: 4,
+      pointBorderColor: '#ffffff',
+      pointBorderWidth: 2,
+      pointBackgroundColor: "rgb(114, 187, 255)",
+      hoverBorderWidth: 3,
+      borderWidth: 2.5,
+      borderColor: "rgba(114, 187, 255, 0.7)"
     }]
   };
 
