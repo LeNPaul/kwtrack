a5743bd5345e132c115cb13e498b1a36755aa631
Fernand <fernandgee@gmail.com>
2018-11-06 18:41:25 -0700

    Dashboard chart: removed background colour fill and made lines wider.

--- members/includes/dashpages/main/assets/dashboard_chart.php
+++ members/includes/dashpages/main/assets/dashboard_chart.php
@@ -3,32 +3,34 @@
 ?>
 
 <script type="text/javascript">
+  Chart.defaults.LineWithLine = Chart.defaults.line;
+  Chart.controllers.LineWithLine = Chart.controllers.line.extend({
+    draw: function(ease) {
+      Chart.controllers.line.prototype.draw.call(this, ease);
+
+      if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
+        var activePoint = this.chart.tooltip._active[0],
+            ctx = this.chart.ctx,
+            x = activePoint.tooltipPosition().x,
+            topY = this.chart.scales['y-axis-0'].top,
+            bottomY = this.chart.scales['y-axis-0'].bottom;
+
+        // draw line
+        ctx.save();
+        ctx.beginPath();
+        ctx.moveTo(x, topY);
+        ctx.lineTo(x, bottomY);
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = '#07C';
+        ctx.stroke();
+        ctx.restore();
+      }
+    }
+  });
+  
   $(function() {
 
-    Chart.defaults.LineWithLine = Chart.defaults.line;
-    Chart.controllers.LineWithLine = Chart.controllers.line.extend({
-      draw: function(ease) {
-        Chart.controllers.line.prototype.draw.call(this, ease);
-
-        if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
-          var activePoint = this.chart.tooltip._active[0],
-              ctx = this.chart.ctx,
-              x = activePoint.tooltipPosition().x,
-              topY = this.chart.scales['y-axis-0'].top,
-              bottomY = this.chart.scales['y-axis-0'].bottom;
-
-          // draw line
-          ctx.save();
-          ctx.beginPath();
-          ctx.moveTo(x, topY);
-          ctx.lineTo(x, bottomY);
-          ctx.lineWidth = 2;
-          ctx.strokeStyle = '#07C';
-          ctx.stroke();
-          ctx.restore();
-        }
-      }
-    });
+    
 
     var start = moment().subtract(59, 'days');
     var end = moment();
