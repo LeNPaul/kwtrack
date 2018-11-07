0e333ccb79a235d2a3e4bd00288a099334b8d338
Fernand <fernandgee@gmail.com>
2018-11-06 19:19:49 -0700

    Dashboard chart: vertical line with snapping on the x-axis

--- members/includes/dashpages/main/assets/dashboard_chart.php
+++ members/includes/dashpages/main/assets/dashboard_chart.php
@@ -63,19 +63,21 @@
       }
     }
   });
+  
+  var dataset = {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+    datasets: [{
+      label: 'Statistics',
+      data: [3, 1, 2, 5, 4, 7, 6],
+      backgroundColor: 'rgba(0, 119, 204, 0.8)',
+      borderColor: 'rgba(0, 119, 204, 0.3)',
+      fill: false
+    }]
+  };
 
   var chart = new Chart(ctx, {
     type: 'LineWithLine',
-    data: {
-      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
-      datasets: [{
-        label: 'Statistics',
-        data: [3, 1, 2, 5, 4, 7, 6],
-        backgroundColor: 'rgba(0, 119, 204, 0.8)',
-        borderColor: 'rgba(0, 119, 204, 0.3)',
-        fill: false
-      }]
-    },
+    data: dataset,
     options: {
       responsive: false,
       tooltips: {
