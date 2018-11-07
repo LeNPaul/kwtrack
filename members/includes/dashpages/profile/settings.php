31642f93f403431b50a15fd711c59d74d9efb35c
Fernand <fernandgee@gmail.com>
2018-11-07 02:29:50 -0700

    Dashboard chart: vertical line with snapping on the x-axis

--- members/includes/dashpages/profile/settings.php
+++ members/includes/dashpages/profile/settings.php
@@ -6,7 +6,7 @@
 <div class="row">
   
   <input type="text" id="keyword" name="keyword" />
-  <button id="#search-btn">Get Volume</button>
+  <button id="search-btn">Get Volume</button>
   
   <p id="exact_volume"></p>
   <p id="broad_volume"></p>
@@ -115,4 +115,67 @@
   </div>
 </div>
 
-<script type="text/javascript" src="/members/includes/dashpages/profile/kw_searchVolume.js"></script>
+<script>
+
+  $(function() {
+
+    $("#search-btn").on("click", function() {
+      console.log("Asdfasdfasdf");
+      var kw = $("#keyword").text();
+      $.ajax({
+        type: "POST",
+        url: "https://sellercentral.amazon.com/sspa/hsa/cm/keywords/power",
+        contentType: "application/json",
+        data: JSON.stringify({
+          pageId: "https://www.amazon.com/HSA/pages/default",
+          keywordList:
+            [{
+              key: kw,
+              matchType: "EXACT"
+            }, {
+              key: kw,
+              matchType: "BROAD"
+            }]
+        }),
+        success: function(r) {
+          console.log(r);
+
+          "object" === ("undefined" == typeof r ? "undefined" : _typeof(r)) && 0 !== r.length || alert("Please sign into your SellerCentral to get the Search Volume data.");
+          var a = !0,
+              i = !1,
+              s = void 0;
+          try {
+            for (var c, u = e[Symbol.iterator](); !(a = (c = u.next()).done); a = !0) {
+              var d = c.value,
+                  p = null,
+                  f = null;
+              try {
+                p = r.find(function(e) {
+                  return e.keyword.toLowerCase() === d[1].toLowerCase() && "EXACT" === e.matchType
+                }), f = r.find(function(e) {
+                  return e.keyword.toLowerCase() === d[1].toLowerCase() && "BROAD" === e.matchType
+                })
+              } catch (h) {}
+              d[d.length - 2] = p ? Math.round(30 * p.impression) : "NONE", d[d.length - 1] = f ? Math.round(30 * f.impression) : "NONE"
+            }
+          } catch (b) {
+            i = !0, s = b
+          } finally {
+            try {
+              !a && u["return"] && u["return"]()
+            } finally {
+              if (i) throw s
+            }
+          }
+          $("#search-btn").prop("disabled", !1), $("#search-btn").html("Check Now"), $("#kw-btn").prop("disabled", !1), $("#kw-btn").html("Relevant KW"), "1" !== localStorage.getItem("amz_isSubscriber") && $(".amz_notSubscribe_table_data").show(), n.width("100%"), t.html("Finished!"), l(e, o)
+        },
+        error: function(d) {
+          console.log(d);
+        }
+      });
+    });
+
+  });
+
+
+</script>
