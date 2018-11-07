8c8c81700aac2ee2c8afa62996f21de746157a23
Fernand <fernandgee@gmail.com>
2018-11-06 18:51:38 -0700

    Dashboard chart: removed background colour fill and made lines wider.

--- members/includes/dashpages/main/dashboard_active5.php
+++ members/includes/dashpages/main/dashboard_active5.php
@@ -313,7 +313,7 @@ $dateArr = array_reverse($dateArr);
 				<!--<canvas id="dummy" width="1000" height="400" style="position: absolute"></canvas>
 				<canvas id="cursor" width="1000" height="400" style="top:0; left:0; position: absolute"></canvas>
 				<canvas id="tooltips" width="1000" height="400" style="top:0;, left:0;, position: absolute"></canvas>-->
-				<canvas id="lineChart" width="1000" height="400" style="top:0; left:0; position: absolute"></canvas>
+				<canvas id="lineChart" width="1000" height="400" ></canvas>
 			</div>
 	  </div>
 
