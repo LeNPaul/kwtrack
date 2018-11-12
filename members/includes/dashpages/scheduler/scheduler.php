007cc91713a5d181fda0ca761b8781ebe0ecf878
Fernand <fernandgee@gmail.com>
2018-11-12 16:03:26 -0700

    Campaign manager updates.

--- members/includes/dashpages/scheduler/scheduler.php
+++ members/includes/dashpages/scheduler/scheduler.php
@@ -8,4 +8,4 @@
 <table id="campaign_list" class="table table-light table-hover row-border order-column" cellpadding="0" cellspacing="0" border="0" width="100%"></table>
 
 
-<script type="text/javascript" src="/includes/dashpages/scheduler/assets/js/index.js"></script>
+<script type="text/javascript" src="includes/dashpages/scheduler/assets/js/index.js"></script>
