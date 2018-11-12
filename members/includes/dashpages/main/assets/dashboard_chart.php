1573f31a5f52ec2cc28e5529acb5202a7a37638b
Fernand <fernandgee@gmail.com>
2018-11-12 13:51:49 -0700

    Dashboard chart frontend updates.

--- members/includes/dashpages/main/assets/dashboard_chart.php
+++ members/includes/dashpages/main/assets/dashboard_chart.php
@@ -139,24 +139,25 @@
 
                 // Create element on first render
                 if (!tooltipEl) {
-                    tooltipEl = document.createElement('div');
-                    tooltipEl.id = 'chartjs-tooltip';
-                    tooltipEl.innerHTML = "<table></table>";
-                    document.body.appendChild(tooltipEl);
+                  tooltipEl = document.createElement('div');
+                  tooltipEl.id = 'chartjs-tooltip';
+                  tooltipEl.innerHTML = "<table></table>";
+                  document.body.appendChild(tooltipEl);
                 }
 
                 // Hide if no tooltip
                 if (tooltipModel.opacity === 0) {
-                    tooltipEl.style.opacity = 0;
-                    return;
+                  tooltipEl.style.opacity = 0;
+                  return;
                 }
 
                 // Set caret Position
                 tooltipEl.classList.remove('above', 'below', 'no-transform');
+
                 if (tooltipModel.yAlign) {
-                    tooltipEl.classList.add(tooltipModel.yAlign);
+                  tooltipEl.classList.add(tooltipModel.yAlign);
                 } else {
-                    tooltipEl.classList.add('no-transform');
+                  tooltipEl.classList.add('no-transform');
                 }
 
                 function getBody(bodyItem) {
@@ -165,28 +166,29 @@
 
                 // Set Text
                 if (tooltipModel.body) {
-                    var titleLines = tooltipModel.title || [];
-                    var bodyLines = tooltipModel.body.map(getBody);
+                  console.log(tooltipModel.body);
+                  var titleLines = tooltipModel.title || [];
+                  var bodyLines = tooltipModel.body.map(getBody);
 
-                    var innerHtml = '<thead>';
+                  var innerHtml = '<thead>';
 
-                    titleLines.forEach(function(title) {
-                        innerHtml += '<tr><th>' + title + '</th></tr>';
-                    });
-                    innerHtml += '</thead><tbody>';
+                  titleLines.forEach(function(title) {
+                      innerHtml += '<tr><th>' + title + '</th></tr>';
+                  });
+                  innerHtml += '</thead><tbody>';
 
-                    bodyLines.forEach(function(body, i) {
-                        var colors = tooltipModel.labelColors[i];
-                        var style = 'background:' + colors.backgroundColor;
-                        style += '; border-color:' + colors.borderColor;
-                        style += '; border-width: 2px';
-                        var span = '<span style="' + style + '"></span>';
-                        innerHtml += '<tr><td>' + span + body + '</td></tr>';
-                    });
-                    innerHtml += '</tbody>';
+                  bodyLines.forEach(function(body, i) {
+                    var colors = tooltipModel.labelColors[i];
+                    var style = 'background:' + colors.backgroundColor;
+                    style += '; border-color:' + colors.borderColor;
+                    style += '; border-width: 2px';
+                    var span = '<span style="' + style + '"></span>';
+                    innerHtml += '<tr><td>' + span + body + '</td></tr>';
+                  });
+                  innerHtml += '</tbody>';
 
-                    var tableRoot = tooltipEl.querySelector('table');
-                    tableRoot.innerHTML = innerHtml;
+                  var tableRoot = tooltipEl.querySelector('table');
+                  tableRoot.innerHTML = innerHtml;
                 }
 
                 // `this` will be the overall tooltip
