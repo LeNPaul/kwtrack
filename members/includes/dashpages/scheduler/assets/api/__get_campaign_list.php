f652b81fcc786d853896758a819af2d4794c76e7
Fernand <fernandgee@gmail.com>
2018-11-12 15:51:48 -0700

    Campaign manager updates.

--- members/includes/dashpages/scheduler/assets/api/__get_campaign_list.php
+++ members/includes/dashpages/scheduler/assets/api/__get_campaign_list.php
@@ -1,4 +1,16 @@
 <?php
 include '../../../../../database/pdo.inc.php';
 
+$user_id = $_POST['user_id'];
+
+$sql = 'SELECT campaign_name FROM campaigns WHERE user_id = ?';
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(1, $user_id, PDO::PARAM_INT);
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<pre>';
+var_dump($result);
+echo '</pre>';
+
 ?>
