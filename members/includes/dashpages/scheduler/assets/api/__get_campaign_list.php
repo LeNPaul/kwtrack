d5921c9926f86980d7e090b2f24ea48eca568f26
Fernand <fernandgee@gmail.com>
2018-11-15 15:41:24 -0700

    Ad Scheduling feature - rendering campaign list

--- members/includes/dashpages/scheduler/assets/api/__get_campaign_list.php
+++ members/includes/dashpages/scheduler/assets/api/__get_campaign_list.php
@@ -3,12 +3,16 @@ include '../../../../../database/pdo.inc.php';
 
 $user_id = 2/*$_POST['user_id']*/;
 
-$sql = 'SELECT campaign_name FROM campaigns WHERE user_id = ?';
+$sql = 'SELECT campaign_name, amz_campaign_id FROM campaigns WHERE user_id = ?';
 $stmt = $pdo->prepare($sql);
 $stmt->bindParam(1, $user_id, PDO::PARAM_INT);
 $stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$campaignNameList = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo json_encode($result, true);
+echo '<pre>';
+var_dump($campaignNameList);
+echo '</pre>';
+
+echo json_encode($campaignNameList, true);
 
 ?>
