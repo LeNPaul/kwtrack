9c74484f474d245f0e0dda37b667ff06aa9201df
Fernand <fernandgee@gmail.com>
2018-11-15 17:19:22 -0700

    Ad Scheduling feature - rendering campaign list

--- members/includes/dashpages/scheduler/scheduler.php
+++ members/includes/dashpages/scheduler/scheduler.php
@@ -5,6 +5,8 @@
 <input id="uid" type="hidden" value="<?= $_SESSION['user_id'] ?>" />
 <h1>Ad Scheduling</h1>
 
+<button id="ad_scheduler" class="btn btn-primary">Edit Ad Schedule</button>
+
 <table id="campaign_list" class="table table-light table-hover table-bordered order-column" cellpadding="0" cellspacing="0" border="0" width="100%"></table>
 
 
