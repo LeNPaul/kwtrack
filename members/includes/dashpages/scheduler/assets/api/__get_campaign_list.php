33d8007747d3cc99a9ed5e87844721770368ce8c
Fernand <fernandgee@gmail.com>
2018-11-18 15:43:58 -0700

    Ad Scheduling feature - datatable modifications

--- members/includes/dashpages/scheduler/assets/api/__get_campaign_list.php
+++ members/includes/dashpages/scheduler/assets/api/__get_campaign_list.php
@@ -12,7 +12,7 @@ $campaignList = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $outputList = [];
 
 for ($i = 0; $i < count($campaignList); $i++) {
-  if ($campaignList[$i]['schedule'] === 0) {
+  if ($campaignList[$i]['schedule'] == 0) {
     $outputList[] = array(
       $campaignList[$i]['campaign_name'],
       '<span class="circle_off" id="' .  $campaignList[$i]['amz_campaign_id'] . '"></span>');
