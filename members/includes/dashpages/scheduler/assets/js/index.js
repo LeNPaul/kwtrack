13c351d007edc0480e40dceb30950f44b94961e8
Fernand <fernandgee@gmail.com>
2018-11-18 13:30:22 -0700

    Ad Scheduling feature

--- members/includes/dashpages/scheduler/assets/js/index.js
+++ members/includes/dashpages/scheduler/assets/js/index.js
@@ -11,7 +11,7 @@ $(function(){
     success: function(campaignList) {
 
       var campaignTableOptions = {
-        dom: 'Blfrtpi',
+        dom: '<"#dt_topBar"lBf>rtpi',
         buttons: [
           'selectAll', 'selectNone'
         ],
