d822f8b51985eabf702d0872e99fe6f197d21fbb
Fernand <fernandgee@gmail.com>
2018-11-18 17:00:04 -0700

    Ad Scheduling feature - datatable modifications

--- members/includes/dashpages/scheduler/scheduler.php
+++ members/includes/dashpages/scheduler/scheduler.php
@@ -12,7 +12,7 @@ if (isset($_POST['campaignIdList'])) {
 <table id="campaign_list" class="table table-light table-hover table-bordered order-column" cellpadding="0" cellspacing="0" border="0" width="100%"></table>
 
 <form id="goToEdit" method="POST">
-  <button name="campaignIdList" id="campaignIdList" class="hidden" type="submit"></button>
+  <button name="campaignIdList" id="campaignIdList" hidden type="submit"></button>
 </form>
 
 <script type="text/javascript" src="includes/dashpages/scheduler/assets/js/index.js"></script>
