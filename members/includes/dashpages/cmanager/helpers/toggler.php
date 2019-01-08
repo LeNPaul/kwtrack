7bc5a56442729933347ec0bdafbc7971b4b508da
Austin Tam <austin.tam7@gmail.com>
2019-01-07 22:01:15 -0500

    bulk action enable/pause change

--- members/includes/dashpages/cmanager/helpers/toggler.php
+++ members/includes/dashpages/cmanager/helpers/toggler.php
@@ -6,19 +6,19 @@ require_once dirname(__FILE__) . '../../../../../database/pdo.inc.php';
 require_once dirname(__FILE__) . '../../../../../database/ElementToggler.php';
 
 use PDO;
+for ($i = 0; $i < count($_POST["element_id"]); $i++) {
+	$config = array(
+		"element_id"     => $_POST["element_id"][$i],
+		"element_name"   => $_POST["element_name"][$i],
+		"data_level"     => $_POST["data_level"],
+		"toggle"         => $_POST["toggle"],
+		"refresh_token"  => $_SESSION["refresh_token"],
+		"profile_id"     => $_SESSION["profileId"]
+	);
 
-$config = array(
-	"element_id"     => $_POST["element_id"],
-	"element_name"   => $_POST["element_name"],
-	"data_level"     => $_POST["data_level"],
-	"toggle"         => $_POST["toggle"],
-	"refresh_token"  => $_SESSION["refresh_token"],
-	"profile_id"     => $_SESSION["profileId"]
-);
+	$toggler = new ElementToggler($config);
+	$toggler->single_toggle();
 
-$toggler = new ElementToggler($config);
-$toggler->single_toggle();
-
-echo $toggler->get_alert_text();
-
+	echo $toggler->get_alert_text();
+}
 ?>
