c3adc9851cdd9c917b067877a037af9df0f5f598
Paul Le <l.nguyen.paul@gmail.com>
2019-01-19 22:09:13 -0500

    Add some inputs to Create a Campaign modal.

--- members/includes/dashpages/cmanager/campaign_manager.php
+++ members/includes/dashpages/cmanager/campaign_manager.php
@@ -17,7 +17,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Create a Campaign</h5>
@@ -26,11 +26,20 @@
         </button>
       </div>
       <div class="modal-body">
-        
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Campaign Name:</label>
+            <input type="text" class="form-control" id="recipient-name" placeholder="Campaign Name">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Daily Budget:</label>
+            <input type="text" class="form-control" id="recipient-name" placeholder="Daily Budget">
+          </div>
+        </form>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-primary" style="margin-right: 10px;">Save changes</button>
       </div>
     </div>
   </div>
