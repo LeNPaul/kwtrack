6569c92857e7574960a0216dbf5827915e35266c
Paul Le <l.nguyen.paul@gmail.com>
2019-01-19 19:41:04 -0500

    Add modal for Create a Campaign button.

--- members/includes/dashpages/cmanager/campaign_manager.php
+++ members/includes/dashpages/cmanager/campaign_manager.php
@@ -2,13 +2,36 @@
 
 <div class="row">
   <div class="col-sm-6">
-    <button class="btn btn-secondary buttons-select-all btn-primary" type=
-    "button">Create a Campaign</button>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+      Create a Campaign
+    </button>
   </div>
   <div class="col-sm-6">
     <div id="campaignRange" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; width: 315px; float: right">
       <i class="fa fa-calendar"></i>
       <span></span> <i class="fa fa-caret-down"></i>
+    </div>
+  </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Create a Campaign</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
     </div>
   </div>
 </div>
