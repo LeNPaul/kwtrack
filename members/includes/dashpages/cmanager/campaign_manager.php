5a34201c332269d30e8f97b06cb058c9780a41ed
Paul Le <l.nguyen.paul@gmail.com>
2019-02-01 23:51:59 -0500

    Add validation form.

--- members/includes/dashpages/cmanager/campaign_manager.php
+++ members/includes/dashpages/cmanager/campaign_manager.php
@@ -89,6 +89,19 @@
                 </div>
               </form>
 
+              <form class="needs-validation" novalidate>
+                <div class="form-row">
+                  <div class="col-md-4 mb-3">
+                    <label for="validationCustom01">First name</label>
+                    <input type="text" class="form-control" id="validationCustom01" placeholder="First name" required>
+                    <div class="valid-feedback">
+                      Looks good!
+                    </div>
+                  </div>
+                </div>
+                <button class="btn btn-primary" type="submit" href="#keyword-1">Submit form</button>
+              </form>
+
         		</div>
         		<div class="tab-pane" id="ad-group-1">
 
